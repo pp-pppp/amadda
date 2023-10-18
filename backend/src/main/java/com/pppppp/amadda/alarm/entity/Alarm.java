ef7277528c9bd80b2cd3e3e9a521225ffdec9618
jeong-yeji <jeongyeji.mail@gmail.com>
2023-10-18 17:38:22 +0900

    feat: Alarm 도메인 초기 설정

--- backend/src/main/java/com/pppppp/amadda/alarm/entity/Alarm.java
+++ backend/src/main/java/com/pppppp/amadda/alarm/entity/Alarm.java
@@ -1,7 +1,34 @@
 package com.pppppp.amadda.alarm.entity;
 
 import com.pppppp.amadda.global.entity.BaseEntity;
+import com.pppppp.amadda.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
+@Entity
 public class Alarm extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long alarmSeq;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_seq")
+    private User user;
+
+    @Column(nullable = false, length = 255)
+    private String content;
+
+    @Column(nullable = false, columnDefinition="TINYINT(1) DEFAULT 0")
+    private boolean isRead;
+
+    @Column(nullable = false, columnDefinition="TINYINT(1) DEFAULT 0")
+    private boolean isSend;
 
 }
---
+++ backend/src/main/java/com/pppppp/amadda/alarm/repository/AlarmConfigRepository.java
@@ -0,0 +1,8 @@
+package com.pppppp.amadda.alarm.repository;
+
+import com.pppppp.amadda.alarm.entity.AlarmConfig;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlarmConfigRepository extends JpaRepository<AlarmConfig, Long> {
+
+}
