3067472e4d112cdf748187b6c0fbc7d1574c904d
donggeoniiii <pkgun0727@gmail.com>
2023-10-18 17:54:23 +0900

    chore: 불필요한 패키지 import 삭제

--- backend/src/main/java/com/pppppp/amadda/global/entity/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/pppppp/amadda/global/entity/exception/GlobalExceptionHandler.java
@@ -6,10 +6,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.pppppp.amadda.global.entity.exception.errorcode.ErrorCode;
 
-import lombok.extern.slf4j.Slf4j;
-
 @RestControllerAdvice
-@Slf4j
 public class GlobalExceptionHandler {
 	@ExceptionHandler(RestApiException.class)
 	public ResponseEntity<Object> handleCustomException(RestApiException e){
