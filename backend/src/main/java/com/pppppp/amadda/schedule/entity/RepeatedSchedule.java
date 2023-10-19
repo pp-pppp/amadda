7e6cbe6d8f218808f47485cb6efed04addadefb6
박동건 Dong-Geon Park <120314184+donggeoniiii@users.noreply.github.com>
2023-10-19 17:21:05 +0900

    Feat: schedule 도메인 entity 초기 설정 (#5)
    * feat: schedule 도메인 entity 초기설정

--- backend/src/main/java/com/pppppp/amadda/schedule/entity/RepeatedSchedule.java
+++ backend/src/main/java/com/pppppp/amadda/schedule/entity/RepeatedSchedule.java
@@ -2,6 +2,21 @@ package com.pppppp.amadda.schedule.entity;
 
 import com.pppppp.amadda.global.entity.BaseEntity;
 
-public class RepeatedSchedule extends BaseEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class RepeatedSchedule extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long repeatedScheduleSeq;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, unique = true, length = 50)
+	private RepeatedFreqency repeatedFrequency;
 }
--- backend/src/main/java/com/pppppp/amadda/schedule/entity/Schedule.java
+++ backend/src/main/java/com/pppppp/amadda/schedule/entity/Schedule.java
@@ -1,7 +1,51 @@
 package com.pppppp.amadda.schedule.entity;
 
+import java.time.LocalDateTime;
+
 import com.pppppp.amadda.global.entity.BaseEntity;
+import com.pppppp.amadda.user.entity.User;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Schedule extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long scheduleSeq;
 
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "user_seq")
+	private User user;
+
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "repeated_schedule_seq")
+	private RepeatedSchedule repeatedSchedule;
+
+	@Column(nullable = false)
+	private boolean isTimeSelected;
+
+	@Column
+	private LocalDateTime scheduleStartAt;
+
+	@Column
+	private LocalDateTime scheduleEndAt;
+
+	@Column(nullable = false)
+	private boolean isAlarmed;
+
+	@Column(nullable = false)
+	private boolean isFinished;
+
+	@Column
+	private String scheduleContent;
+
+	@Column(nullable = false)
+	private boolean isAuthorizedAll;
 }
