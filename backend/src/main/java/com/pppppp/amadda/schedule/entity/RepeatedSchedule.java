55d2e13442ec5320b07d739ecd4856bf252c4152
donggeoniiii <pkgun0727@gmail.com>
2023-10-19 16:27:50 +0900

    chore: 불필요한 annotation 삭제

--- backend/src/main/java/com/pppppp/amadda/schedule/entity/RepeatedSchedule.java
+++ backend/src/main/java/com/pppppp/amadda/schedule/entity/RepeatedSchedule.java
@@ -9,20 +9,8 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
-@Table
-@Getter
-@Setter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class RepeatedSchedule extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
