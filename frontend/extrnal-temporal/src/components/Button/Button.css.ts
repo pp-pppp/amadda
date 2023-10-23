060bc81ef49c97ec96fbf2e70623c1b26c1ca0a9
brewcoldblue <seung.yoon.yu@gmail.com>
2023-10-23 15:58:12 +0900

    feat: 예제 컴포넌트 추가

--- frontend/extrnal-temporal/src/components/Button/Button.css.ts
+++ frontend/extrnal-temporal/src/components/Button/Button.css.ts
@@ -1,5 +1,10 @@
-import { style } from "@vanilla-extract/css";
+import { style } from '@vanilla-extract/css';
 
-export const base = style(
-	{color: "yellow"}
-)
+export const base = style({
+  color: 'blue',
+  backgroundColor: 'white',
+  border: '1px solid black',
+  padding: '0.75rem',
+  borderRadius: '1rem',
+  cursor: 'pointer',
+});
--- frontend/extrnal-temporal/src/components/Button/Button.tsx
+++ frontend/extrnal-temporal/src/components/Button/Button.tsx
@@ -1,5 +1,8 @@
-import { base } from "./Button.css";
-
-export default function Button() {
-	return <button className={base}>ㅋㅋㅋㅋ</button>
+import { HTMLAttributes } from 'react';
+import { base } from './Button.css';
+interface Props extends HTMLAttributes<HTMLButtonElement> {
+  label: string;
+}
+export default function Button({ label }: Props) {
+  return <button className={base}>{label}</button>;
 }
