c7d32ac08f9be7a1626adfb781884d52e1efb444
Jeong Yeji <jeongyeji.mail@gmail.com>
2023-10-25 10:48:20 +0900

    Feat: 프로젝트 세팅 (#11)
    * feat: JpaAuditing 설정 분리
    
    * feat: 설정 파일 properties에서 yml로 변경
    
    * feat: ApiResponse 생성
    
    * feat: data.sql 생성
    
    * test: 테스트 환경 통합 설정
    
    * chore: gitignore 수정

--- backend/src/main/java/com/pppppp/amadda/AmaddaApplication.java
+++ backend/src/main/java/com/pppppp/amadda/AmaddaApplication.java
@@ -1,11 +1,15 @@
 package com.pppppp.amadda;
 
+import com.pppppp.amadda.config.EnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@PropertySource(value = {
+    "classpath:env.yml",
+}, factory = EnvConfig.class)
 public class AmaddaApplication {
 
     public static void main(String[] args) {
---
+++ backend/src/test/java/com/pppppp/amadda/ControllerTestSupport.java
@@ -0,0 +1,9 @@
+package com.pppppp.amadda;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(controllers = {
+})
+public abstract class ControllerTestSupport {
+
+}
