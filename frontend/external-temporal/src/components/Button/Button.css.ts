ccebda97f5fb74c80a634a1e53717af634ceed1b
unknown <khappy517@gmail.com>
2023-10-25 14:05:22 +0900

    test: storybook 예시 파일 수정

--- frontend/external-temporal/src/components/Button/Button.css.ts
+++ frontend/external-temporal/src/components/Button/Button.css.ts
@@ -1,7 +1,20 @@
 import { style } from '@vanilla-extract/css';
+// import colors from '../../../src/constants/colors';
+import colors from '@/constants/colors';
 
 export const base = style({
-  color: 'blue',
+  color: colors.key,
+  fontWeight: 400,
+  backgroundColor: 'white',
+  border: '1px solid black',
+  padding: '0.75rem',
+  borderRadius: '1rem',
+  cursor: 'pointer',
+});
+
+export const test = style({
+  color: colors.key,
+  fontWeight: 700,
   backgroundColor: 'white',
   border: '1px solid black',
   padding: '0.75rem',
--- frontend/external-temporal/src/components/Button/Button.tsx
+++ frontend/external-temporal/src/components/Button/Button.tsx
@@ -1,8 +1,13 @@
 import { HTMLAttributes } from 'react';
-import { base } from './Button.css';
+import { base, test } from './Button.css';
 interface Props extends HTMLAttributes<HTMLButtonElement> {
   label: string;
 }
 export default function Button({ label }: Props) {
-  return <button className={base}>{label}</button>;
+  return (
+    <>
+      <button className={base}>{label}</button>
+      <button className={test}>{label}</button>
+    </>
+  );
 }
