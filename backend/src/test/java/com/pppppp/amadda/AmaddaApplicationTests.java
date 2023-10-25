54a668444891bd44e804c95286783f4fbed8b7cb
jeong-yeji <jeongyeji.mail@gmail.com>
2023-10-25 10:14:41 +0900

    test: 테스트 환경 통합 설정

--- backend/src/test/java/com/pppppp/amadda/AmaddaApplicationTests.java
+++ backend/src/test/java/com/pppppp/amadda/AmaddaApplicationTests.java
@@ -3,8 +3,7 @@ package com.pppppp.amadda;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class AmaddaApplicationTests {
+class AmaddaApplicationTests extends IntegrationTestSupport {
 
     @Test
     void contextLoads() {
