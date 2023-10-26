7dcb3b1f5a82dc2a4999cac9a5b54c0815342d00
박동건 Dong-Geon Park <120314184+donggeoniiii@users.noreply.github.com>
2023-10-26 17:33:27 +0900

    Feat: Schedule 도메인 초기 설정 (#26)
    * feat: Schedule Layer 초기 설정

---
+++ backend/src/main/java/com/pppppp/amadda/schedule/controller/ScheduleController.java
@@ -0,0 +1,44 @@
+package com.pppppp.amadda.schedule.controller;
+
+import com.pppppp.amadda.schedule.service.ScheduleService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/schedule")
+@RequiredArgsConstructor
+@Slf4j
+public class ScheduleController {
+
+    private final ScheduleService scheduleService;
+
+    @PostMapping("")
+    public void createSchedule() {
+        log.info("POST /api/schedule");
+    }
+
+    @GetMapping("/{scheduleSeq}")
+    public void getSchedule(@PathVariable Long scheduleSeq) {
+        log.info("GET /api/schedule/" + scheduleSeq);
+    }
+
+    @GetMapping("")
+    public void getSchedule(@RequestParam(value = "category", required = false) Long categorySeq,
+        @RequestParam(value = "searchKey", required = false) String searchKey,
+        @RequestParam(value = "unscheduled", required = false) Long unscheduled) {
+        log.info("GET /api/schedule?category={}&searchKey={}&unscheduled={}", categorySeq,
+            searchKey, unscheduled);
+    }
+
+    @GetMapping("/{scheduleSeq}/participation")
+    public void getParticipatingUsers(@PathVariable Long scheduleSeq,
+        @RequestParam(value = "searchKey", required = false) String searchKey) {
+        log.info("GET /api/schedule/{}/participation?searchKey={}", scheduleSeq, searchKey);
+    }
+}
