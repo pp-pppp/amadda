186346ba8e6249f8db766163d2982378332b5bd8
mindy0414 <mmminjung0414@gmail.com>
2023-10-26 13:55:38 +0900

    Feat: 유저, 친구 세팅

---
+++ backend/src/main/java/com/pppppp/amadda/friend/repository/FriendRepository.java
@@ -0,0 +1,10 @@
+package com.pppppp.amadda.friend.repository;
+
+import com.pppppp.amadda.friend.entity.Friend;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FriendRepository extends JpaRepository<Friend, Long> {
+
+
+
+}
