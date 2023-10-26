8ff8a735e85d671fa7d919e26ce93502cdc25f4b
brewcoldblue <seung.yoon.yu@gmail.com>
2023-10-26 10:20:03 +0900

    feat: 전역 스타일 추가 및 예제 코드 삭제

--- frontend/external-temporal/src/global.css.ts
+++ frontend/external-temporal/src/global.css.ts
@@ -1,4 +1,5 @@
 import { globalFontFace, globalStyle } from '@vanilla-extract/css';
+import colors from './constants/colors';
 
 globalFontFace('pretendard', {
   src: 'url("/fonts/Pretendard-Regular.woff")',
@@ -10,6 +11,38 @@ globalFontFace('pretendardBold', {
   fontWeight: 700,
 });
 
-globalStyle('html, body', {
-  backgroundColor: 'pink',
+globalStyle('html, body, h1, h2, h3, h4, h5, h6, p, span', {
+  fontFamily: 'pretendard',
+  margin: 0,
+  padding: 0,
+  fontWeight: 400,
+  fontSize: '100%',
+  lineHeight: '1',
+  color: colors.BLACK,
+});
+
+globalStyle('*', {
+  boxSizing: 'border-box',
+});
+
+globalStyle('a', {
+  textDecoration: 'none',
+  color: 'inherit',
+});
+
+globalStyle('ul, li', {
+  margin: 0,
+  padding: 0,
+  listStyleType: 'none',
+});
+
+globalStyle('img, svg', {
+  verticalAlign: 'middle',
+});
+
+globalStyle('textarea', {
+  fontFamily: 'pretendard',
+  resize: 'none',
+  margin: 0,
+  padding: 0,
 });
