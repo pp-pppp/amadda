5d105e98ff917000f31b139d9033a0dd177e342d
brewcoldblue <seung.yoon.yu@gmail.com>
2023-10-26 15:08:15 +0900

    feat: Spaging(공간) 컴포넌트 추가

---
+++ frontend/external-temporal/src/components/atom/Spacing/Spacing.css.ts
@@ -0,0 +1,42 @@
+import { styleVariants } from '@vanilla-extract/css';
+
+export const VSIZE = styleVariants({
+  '0.5': {
+    height: '0.5rem',
+  },
+  '1': {
+    height: '1rem',
+  },
+  '2': {
+    height: '2rem',
+  },
+  '3': {
+    height: '3rem',
+  },
+  '5': {
+    height: '5rem',
+  },
+  '10': {
+    height: '10rem',
+  },
+});
+export const HSIZE = styleVariants({
+  '0.5': {
+    width: '0.5rem',
+  },
+  '1': {
+    width: '1rem',
+  },
+  '2': {
+    width: '2rem',
+  },
+  '3': {
+    width: '3rem',
+  },
+  '5': {
+    width: '5rem',
+  },
+  '10': {
+    width: '10rem',
+  },
+});
