4f4535333f167500ae8098efc9d82d8c588853e9
unknown <khappy517@gmail.com>
2023-10-26 14:35:46 +0900

    feat: Input(text, checkbox) 컴포넌트 추가

---
+++ frontend/external-temporal/src/components/atom/Input/Input.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { ChangeEvent, HTMLAttributes } from 'react';
+import { TYPE_VARIANTS } from './Input.css';
+
+interface Props extends HTMLAttributes<HTMLInputElement> {
+  type: 'text' | 'checkbox';
+  validator?(target: string): boolean;
+  id: string;
+  name: string;
+  onChange(e: React.ChangeEvent<HTMLInputElement>): void;
+  disabled: boolean;
+  placeholder?: string;
+  value: string;
+  checked?: boolean;
+}
+
+export default function Input({
+  type,
+  validator = target => true,
+  id,
+  name,
+  onChange,
+  disabled,
+  placeholder = '',
+  value,
+  checked,
+}: Props) {
+  return (
+    <input
+      className={TYPE_VARIANTS[type]}
+      type={type}
+      id={id}
+      name={name}
+      onChange={e => validator(e.target.value) && onChange(e)}
+      disabled={disabled}
+      placeholder={placeholder}
+      value={value}
+      checked={checked}
+    ></input>
+  );
+}
