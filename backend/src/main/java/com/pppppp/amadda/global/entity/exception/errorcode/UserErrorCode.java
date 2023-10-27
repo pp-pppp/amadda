2e93e00b87bbef7ca28e88a4c7fd4cba1d0c92a9
mindy0414 <mmminjung0414@gmail.com>
2023-10-27 13:02:26 +0900

    feat: 유저 테스트&서비스&레포 코드 작성

---
+++ backend/src/main/java/com/pppppp/amadda/global/entity/exception/errorcode/UserErrorCode.java
@@ -0,0 +1,16 @@
+package com.pppppp.amadda.global.entity.exception.errorcode;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserErrorCode implements ErrorCode {
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 유저 없음")
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+}
--- backend/src/main/java/com/pppppp/amadda/user/service/UserService.java
+++ backend/src/main/java/com/pppppp/amadda/user/service/UserService.java
@@ -1,13 +1,23 @@
 package com.pppppp.amadda.user.service;
 
+import com.pppppp.amadda.user.dto.response.UserReadResponse;
+import com.pppppp.amadda.user.entity.User;
 import com.pppppp.amadda.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserService {
 
     private final UserRepository userRepository;
+
+    public Optional<User> findUserByUserSeq(Long userSeq) {
+        return Optional.ofNullable(userSeq).flatMap(userRepository::findByUserSeq);
+    }
 
 }
