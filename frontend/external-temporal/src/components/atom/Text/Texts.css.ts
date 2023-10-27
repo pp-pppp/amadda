e57f7847569e354a0bed17234e65a21c05c81ef1
brewcoldblue <seung.yoon.yu@gmail.com>
2023-10-27 14:10:06 +0900

    feat: grey, lightgrey 색상 추가

--- frontend/external-temporal/src/components/atom/Text/Texts.css.ts
+++ frontend/external-temporal/src/components/atom/Text/Texts.css.ts
@@ -4,6 +4,8 @@ import { styleVariants } from '@vanilla-extract/css';
 export const COLOR = styleVariants({
   key: { color: colors.key },
   black: { color: colors.black },
+  grey: { color: colors.GREY_600 },
+  lightgrey: { color: colors.GREY_400 },
   white: { color: colors.white },
   warn: { color: colors.category.salmon },
   ok: { color: colors.category.green },
