54e7b1a6501744e26153c8e63970c21938a2d56d
mindy0414 <mmminjung0414@gmail.com>
2023-10-27 14:22:24 +0900

    fix: 예외테스트 코드 추가, 에외처리 메소드 추가

--- backend/src/main/java/com/pppppp/amadda/user/service/UserService.java
+++ backend/src/main/java/com/pppppp/amadda/user/service/UserService.java
@@ -1,5 +1,7 @@
 package com.pppppp.amadda.user.service;
 
+import com.pppppp.amadda.global.entity.exception.RestApiException;
+import com.pppppp.amadda.global.entity.exception.errorcode.UserErrorCode;
 import com.pppppp.amadda.user.dto.response.UserReadResponse;
 import com.pppppp.amadda.user.entity.User;
 import com.pppppp.amadda.user.repository.UserRepository;
@@ -15,6 +17,11 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    public User getUserBySeq(Long userSeq) {
+        return findUserByUserSeq(userSeq)
+                .orElseThrow(() -> new RestApiException(UserErrorCode.USER_NOT_FOUND));
+    }
 
     public Optional<User> findUserByUserSeq(Long userSeq) {
         return Optional.ofNullable(userSeq).flatMap(userRepository::findByUserSeq);
--- backend/src/test/java/com/pppppp/amadda/user/service/UserServiceTest.java
+++ backend/src/test/java/com/pppppp/amadda/user/service/UserServiceTest.java
@@ -2,8 +2,6 @@ package com.pppppp.amadda.user.service;
 
 import com.pppppp.amadda.IntegrationTestSupport;
 import com.pppppp.amadda.global.entity.exception.RestApiException;
-import com.pppppp.amadda.global.entity.exception.errorcode.UserErrorCode;
-import com.pppppp.amadda.user.dto.response.UserReadResponse;
 import com.pppppp.amadda.user.entity.User;
 import com.pppppp.amadda.user.repository.UserRepository;
 import org.junit.jupiter.api.AfterEach;
@@ -14,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class UserServiceTest extends IntegrationTestSupport {
 
@@ -30,19 +28,28 @@ class UserServiceTest extends IntegrationTestSupport {
 
     @DisplayName("userSeq로 유저를 조회할 수 있다.")
     @Test
-    void findUserByUserSeq() {
+    void getUserByUserSeq() {
         // given
         User u1 = User.create(1111l, "유저1", "id1", "imageUrl1", false);
         User u2 = User.create(1234l, "유저2", "id2", "imageUrl2", true);
         userRepository.saveAll(List.of(u1, u2));
 
         // when
-        User u = userService.findUserByUserSeq(1111l)
-                .orElseThrow(() -> new RestApiException(UserErrorCode.USER_NOT_FOUND));
+        User u = userService.getUserBySeq(1111l);
 
         // then
         assertThat(u).isNotNull();
         assertThat(u).extracting("userSeq", "userName", "userId", "imageUrl", "isInited")
                 .containsExactlyInAnyOrder(1111l, "유저1", "id1", "imageUrl1", false);
+    }
+
+    @DisplayName("존재하지 않는 userSeq로 유저를 조회하면 예외가 발생한다. ")
+    @Test
+    void getUnexpectedUserByUserSeq() {
+
+        // when // then
+        assertThatThrownBy(() -> userService.getUserBySeq(1111l))
+                .isInstanceOf(RestApiException.class);
+                // 추후 코드랑 메시지 확인 코드 추가 예정
     }
 }
