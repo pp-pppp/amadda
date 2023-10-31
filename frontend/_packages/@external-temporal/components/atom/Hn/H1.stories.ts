7cd77444ce5e7941789753a1ada67dd96c10ca88
Sohee Kwon <khappy517@gmail.com>
2023-10-31 18:00:25 +0900

    docs: H1 stories docs 수정

--- frontend/_packages/@external-temporal/components/atom/Hn/H1.stories.ts
+++ frontend/_packages/@external-temporal/components/atom/Hn/H1.stories.ts
@@ -6,10 +6,19 @@ const meta = {
   title: 'atom/Headings/H1',
   component: H1,
   parameters: {
-    // layout: 'centered',
+    componentSubtitle: 'H1 요소',
+    docs: {
+      description: {
+        component: '커스텀한 h1 태그입니다.',
+      },
+    },
   },
   tags: ['autodocs'],
-  argTypes: {},
+  argTypes: {
+    children: {
+      description: '자식 노드를 받습니다. 텍스트 노드 사용을 권고합니다.',
+    },
+  },
 } satisfies Meta<typeof H1>;
 
 export default meta;
@@ -17,7 +26,6 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
   args: {
-    // primary: true,
     children: '안녕 나는 제목이야',
   },
 };
