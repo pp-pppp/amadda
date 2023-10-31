008f9dd4883b1791ddc8337d65b8f1e0e19f8b4d
Sohee Kwon <khappy517@gmail.com>
2023-10-31 18:01:47 +0900

    docs: H5 stories docs 수정

--- frontend/_packages/@external-temporal/components/atom/Hn/H5.stories.ts
+++ frontend/_packages/@external-temporal/components/atom/Hn/H5.stories.ts
@@ -6,10 +6,19 @@ const meta = {
   title: 'atom/Headings/H5',
   component: H5,
   parameters: {
-    // layout: 'centered',
+    componentSubtitle: 'H5 요소',
+    docs: {
+      description: {
+        component: '커스텀한 h5 태그입니다.',
+      },
+    },
   },
   tags: ['autodocs'],
-  argTypes: {},
+  argTypes: {
+    children: {
+      description: '자식 노드를 받습니다. 텍스트 노드 사용을 권고합니다.',
+    },
+  },
 } satisfies Meta<typeof H5>;
 
 export default meta;
@@ -17,7 +26,6 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
   args: {
-    // primary: true,
     children: '안녕 나는 다섯 번째 제목이야',
   },
 };
