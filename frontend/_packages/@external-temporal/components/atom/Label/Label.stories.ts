7cf024552772b933ff9fbd96a97c4e7469db1097
Sohee Kwon <khappy517@gmail.com>
2023-10-31 18:18:26 +0900

    docs: Input stories docs 수정

--- frontend/_packages/@external-temporal/components/atom/Label/Label.stories.ts
+++ frontend/_packages/@external-temporal/components/atom/Label/Label.stories.ts
@@ -1,16 +1,29 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from '@storybook/react';
 
-import { Label } from "./Label";
+import { Label } from './Label';
 
 const meta = {
-  title: "atom/Label",
+  title: 'atom/Label',
   component: Label,
   parameters: {
-    layout: "centered",
+    layout: 'centered',
+    componentSubtitle: 'input label',
+    docs: {
+      description: {
+        component: '바인딩 될 input 태그의 상단에 보여질 label입니다.',
+      },
+    },
   },
-  tags: ["autodocs"],
-  argTypes: {},
+  tags: ['autodocs'],
+  argTypes: {
+    htmlFor: {
+      description: '바인딩 될 input 태그의 id입니다.',
+    },
+    children: {
+      description: '자식 노드를 받습니다. 텍스트 노드 사용을 권고합니다.',
+    },
+  },
 } satisfies Meta<typeof Label>;
 
 export default meta;
@@ -18,7 +31,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Text: Story = {
   args: {
-    htmlFor: "sample-text",
-    children: "이메일을 입력해주세요.",
+    htmlFor: 'sample-text',
+    children: '이메일을 입력해주세요.',
   },
 };
