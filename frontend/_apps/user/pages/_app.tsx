d5f2b33c92d258e2907f4825795c35f220c83108
Sohee Kwon <khappy517@gmail.com>
2023-10-31 11:11:18 +0900

    feat: next app에 global css 적용

--- frontend/_apps/user/pages/_app.tsx
+++ frontend/_apps/user/pages/_app.tsx
@@ -1,5 +1,6 @@
 // import dynamic from 'next/dynamic';
-import { lazy } from 'react';
+import { lazy } from "react";
+import "#/util/global.css";
 
 function App({ Component, pageProps }) {
   return (
