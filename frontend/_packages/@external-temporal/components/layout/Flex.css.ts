f794e0f16ab843100617ab52e7675d88e7251b8e
brewcoldblue <seung.yoon.yu@gmail.com>
2023-10-31 15:47:51 +0900

    test: Flex storybook 파일 추가

---
+++ frontend/_packages/@external-temporal/components/layout/Flex.css.ts
@@ -0,0 +1,26 @@
+import { styleVariants } from '@vanilla-extract/css';
+
+export const DIRECTION_VARIANT = styleVariants({
+  row: { display: 'flex', flexDirection: 'row' },
+  column: { display: 'flex', flexDirection: 'column' },
+});
+
+export const JUSTIFY_VARIANT = styleVariants({
+  start: { justifyContent: 'start' },
+  center: { justifyContent: 'center' },
+  spaceBetween: { justifyContent: 'space-between' },
+  spaceAround: { justifyContent: 'space-around' },
+  spaceEvenly: { justifyContent: 'space-evenly' },
+});
+
+export const ALIGN_VARIANT = styleVariants({
+  start: { alignItems: 'start' },
+  center: { alignItems: 'center' },
+  end: { alignItems: 'end' },
+  stretch: { alignItems: 'stretch' },
+});
+
+export const WRAP_VARIANT = styleVariants({
+  none: { flexWrap: 'nowrap' },
+  wrap: { flexWrap: 'wrap' },
+});
---
+++ frontend/_packages/@external-temporal/components/layout/Flex.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  ALIGN_VARIANT,
+  DIRECTION_VARIANT,
+  JUSTIFY_VARIANT,
+  WRAP_VARIANT,
+} from './Flex.css';
+export interface FlexProps {
+  flexDirection?: keyof typeof DIRECTION_VARIANT;
+  justifyContents: keyof typeof JUSTIFY_VARIANT;
+  alignItems: keyof typeof ALIGN_VARIANT;
+  flexWrap?: keyof typeof WRAP_VARIANT;
+  children: React.ReactNode;
+}
+
+export function Flex({
+  flexDirection = 'row',
+  justifyContents,
+  alignItems = 'center',
+  flexWrap = 'wrap',
+  children,
+}: FlexProps) {
+  const className = `${DIRECTION_VARIANT[flexDirection]} ${JUSTIFY_VARIANT[justifyContents]} ${ALIGN_VARIANT[alignItems]} ${WRAP_VARIANT[flexWrap]}`;
+  return <div className={className}>{children}</div>;
+}
+
+export default Flex;
