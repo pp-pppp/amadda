9c6c9a9b0a134b7eb3d6cb4f12bfa5c9bc0b3a2f
Sohee Kwon <khappy517@gmail.com>
2023-10-31 17:53:07 +0900

    docs: BtnRound stories docs 수정

--- frontend/_packages/@external-temporal/components/atom/BtnRound/BtnRound.stories.ts
+++ frontend/_packages/@external-temporal/components/atom/BtnRound/BtnRound.stories.ts
@@ -7,9 +7,36 @@ const meta = {
   component: BtnRound,
   parameters: {
     layout: 'centered',
+    componentSubtitle: '둥근 버튼',
+    docs: {
+      description: {
+        component: '둥근 버튼입니다.',
+      },
+    },
   },
   tags: ['autodocs'],
-  argTypes: {},
+  argTypes: {
+    type: {
+      description:
+        '버튼의 타입을 선택합니다. form 태그 내부의 button은 submit을 선택해주세요.',
+    },
+    variant: {
+      description: '버튼의 색상을 지정할 수 있습니다.',
+    },
+    size: {
+      description: '버튼의 크기를 지정할 수 있습니다.',
+    },
+    disabled: {
+      description:
+        '버튼의 작동 여부를 정합니다. 클릭을 막고 싶다면 true를 선택해주세요.',
+    },
+    children: {
+      description: '자식 노드를 받습니다. 텍스트 노드 사용을 권고합니다.',
+    },
+    onClick: {
+      description: '클릭 시 실행될 함수입니다.',
+    },
+  },
 } satisfies Meta<typeof BtnRound>;
 
 export default meta;
@@ -17,7 +44,6 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
   args: {
-    // primary: true,
     type: 'button',
     variant: 'key',
     disabled: false,
