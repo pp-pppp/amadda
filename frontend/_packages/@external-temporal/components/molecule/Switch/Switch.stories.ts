d2b1c6391f5078cec2f5734c005a1c72306eb92e
brewcoldblue <82228797+brewcoldblue@users.noreply.github.com>
2023-10-31 17:07:34 +0900

    Feat: Switch 컴포넌트 추가 (#50)
    * feat: Switch 컴포넌트 추가
    
    * test: Flex storybook 파일 추가
    
    * feat: flexWrap의 key를 none에서 nowrap으로 변경
    
    * fix: value 바인딩 및 리팩토링
    
    * docs: 잘못된 prop 설명 삭제

---
+++ frontend/_packages/@external-temporal/components/molecule/Switch/Switch.stories.ts
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Switch } from './Switch';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: 'molecule/Switch',
+  component: Switch,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'centered',
+    componentSubtitle: '한 줄 요약',
+    docs: {
+      description: {
+        component: '컴파운드 컴포넌트인 경우 여기서 설명합니다.',
+      },
+    },
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    // backgroundColor: { control: 'color' },
+  },
+} satisfies Meta<typeof Switch>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+  args: {
+    // primary: true,
+    id: 'id',
+    selected: true,
+    value: 'wow',
+  },
+};
---
+++ frontend/_packages/@external-temporal/components/molecule/Switch/Switch.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { HTMLAttributes } from 'react';
+import {
+  CONTAINER_BASE,
+  CONTAINER_VARIANT,
+  SLIDER,
+  SWITCH_BASE,
+  SWITCH_VARIANT,
+} from './Switch.css';
+interface SwitchProps extends HTMLAttributes<HTMLInputElement> {
+  id: string;
+  selected: boolean;
+  value: string;
+}
+export function Switch({ id, selected, value }: SwitchProps) {
+  const isSelected = selected ? 'selected' : 'unselected';
+  const CONTAINER = `${CONTAINER_BASE} ${CONTAINER_VARIANT[isSelected]}`;
+  const SWITCH = `${SWITCH_BASE} ${SWITCH_VARIANT[isSelected]}`;
+  return (
+    <div className={CONTAINER}>
+      <div className={SLIDER}>
+        <input className={SWITCH} id={id} type="checkbox" value={value} />
+      </div>
+    </div>
+  );
+}
