d05bbad5ab4cfed10a6f255f818a67e1f57f2b83
Sohee Kwon <khappy517@gmail.com>
2023-10-31 12:55:25 +0900

    chore: 불필요한 파일 삭제

--- frontend/_apps/schedule/.prettier.c.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  extends: "prettier-config/base.js",
-};
--- frontend/_packages/prettier-config/base.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  printWidth: 80,
-  singleQuote: true,
-  trailingComma: "es5",
-  tabWidth: 2,
-  arrowParens: "avoid",
-};
