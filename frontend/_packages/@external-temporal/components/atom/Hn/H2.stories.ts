f90cffa60b525528c2de37b1ec13a1d8a91fff90
Sohee Kwon <khappy517@gmail.com>
2023-10-31 18:01:16 +0900

    docs: H2 stories docs 수정

--- frontend/_packages/@external-temporal/components/atom/Hn/H2.stories.ts
+++ frontend/_packages/@external-temporal/components/atom/Hn/H2.stories.ts
@@ -6,10 +6,19 @@ const meta = {
   title: 'atom/Headings/H2',
   component: H2,
   parameters: {
-    // layout: 'centered',
+    componentSubtitle: 'H2 요소',
+    docs: {
+      description: {
+        component: '커스텀한 h2 태그입니다.',
+      },
+    },
   },
   tags: ['autodocs'],
-  argTypes: {},
+  argTypes: {
+    children: {
+      description: '자식 노드를 받습니다. 텍스트 노드 사용을 권고합니다.',
+    },
+  },
 } satisfies Meta<typeof H2>;
 
 export default meta;
@@ -17,7 +26,6 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
   args: {
-    // primary: true,
     children: '안녕 나는 두 번째 제목이야',
   },
 };
