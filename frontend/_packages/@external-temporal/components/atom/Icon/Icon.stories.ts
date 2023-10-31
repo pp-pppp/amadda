e96712fa67bb9acda8828a081f26dbd0e9ca52ac
Sohee Kwon <khappy517@gmail.com>
2023-10-31 18:07:37 +0900

    docs: Icon stories docs 수정

--- frontend/_packages/@external-temporal/components/atom/Icon/Icon.stories.ts
+++ frontend/_packages/@external-temporal/components/atom/Icon/Icon.stories.ts
@@ -6,10 +6,26 @@ const meta = {
   title: 'atom/Icon',
   component: Icon,
   parameters: {
-    // layout: 'centered',
+    componentSubtitle: '아이콘',
+    docs: {
+      description: {
+        component: 'svg로 구성된 아이콘입니다.',
+      },
+    },
   },
   tags: ['autodocs'],
-  argTypes: {},
+  argTypes: {
+    type: {
+      description: '사용하려는 아이콘을 선택할 수 있습니다.',
+    },
+    color: {
+      description: '아이콘의 색상을 선택할 수 있습니다.',
+    },
+    cursor: {
+      description:
+        '커서 종류를 선택할 수 있습니다. 기본은 화살표로, pointer를 선택하면 손가락 모양으로 커서가 변경됩니다.',
+    },
+  },
 } satisfies Meta<typeof Icon>;
 
 export default meta;
