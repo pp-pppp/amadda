09c9e016a277f16190bc5adf0cdd13dd7782f6e6
Sohee Kwon <khappy517@gmail.com>
2023-10-31 18:01:56 +0900

    docs: H6 stories docs 수정

--- frontend/_packages/@external-temporal/components/atom/Hn/H6.stories.ts
+++ frontend/_packages/@external-temporal/components/atom/Hn/H6.stories.ts
@@ -6,10 +6,19 @@ const meta = {
   title: 'atom/Headings/H6',
   component: H6,
   parameters: {
-    // layout: 'centered',
+    componentSubtitle: 'H6 요소',
+    docs: {
+      description: {
+        component: '커스텀한 h6 태그입니다.',
+      },
+    },
   },
   tags: ['autodocs'],
-  argTypes: {},
+  argTypes: {
+    children: {
+      description: '자식 노드를 받습니다. 텍스트 노드 사용을 권고합니다.',
+    },
+  },
 } satisfies Meta<typeof H6>;
 
 export default meta;
@@ -17,7 +26,6 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
   args: {
-    // primary: true,
     children: '안녕 나는 여섯 번째 제목이야',
   },
 };
