01a05148bf4dd0a8236a8f5c7a0a48f6de8c99f8
jeong-yeji <jeongyeji.mail@gmail.com>
2023-11-01 16:13:03 +0900

    feat: 알림 변수, 메시지, 토픽 설정

--- backend/src/main/java/com/pppppp/amadda/alarm/entity/AlarmContent.java
+++ backend/src/main/java/com/pppppp/amadda/alarm/entity/AlarmContent.java
@@ -5,15 +5,24 @@ import lombok.Getter;
 @Getter
 public enum AlarmContent {
 
-    TEST(AlarmType.TEST, "테스트용")
+    FRIEND_REQUEST(AlarmType.FRIEND_REQUEST, "%s 님이 친구신청을 했어요."),
+    FRIEND_ACCEPT(AlarmType.FRIEND_ACCEPT, "%s 님이 친구신청을 수락했어요."),
+    SCHEDULE_ASSIGNED(AlarmType.SCHEDULE_ASSIGNED, "%s 님이 '%s' 일정을 추가했어요."),
+    MENTIONED(AlarmType.MENTIONED, "%s 님이 '%s' 일정에서 당신을 멘션했어요."),
+    SCHEDULE_UPDATE(AlarmType.SCHEDULE_UPDATE, "'%s' 일정이 수정되었어요."),
+    SCHEDULE_NOTI(AlarmType.SCHEDULE_NOTI, "'%s' 일정이 %s 예정되어 있어요."),
     ;
 
     private final String type;
-    private final String content;
+    private final String message;
 
-    AlarmContent(AlarmType alarmType, String content) {
+    AlarmContent(AlarmType alarmType, String message) {
         this.type = alarmType.getCode();
-        this.content = content;
+        this.message = message;
+    }
+
+    public String getMessage(String... parameter) {
+        return String.format(message, parameter);
     }
 
 }
---
+++ backend/src/test/java/com/pppppp/amadda/alarm/entity/AlarmContentTest.java
@@ -0,0 +1,73 @@
+package com.pppppp.amadda.alarm.entity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.MissingFormatArgumentException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class AlarmContentTest {
+
+    @DisplayName("friend_request")
+    @Test
+    void friend_request() {
+        String expected = "정예지 님이 친구신청을 했어요.";
+        String actual = AlarmContent.FRIEND_REQUEST.getMessage("정예지");
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("friend_accept")
+    @Test
+    void friend_accept() {
+        String expected = "정예지 님이 친구신청을 수락했어요.";
+        String actual = AlarmContent.FRIEND_ACCEPT.getMessage("정예지");
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("schedule_assigned")
+    @Test
+    void schedule_assigned() {
+        String expected = "정예지 님이 '밥먹자' 일정을 추가했어요.";
+        String actual = AlarmContent.SCHEDULE_ASSIGNED.getMessage("정예지", "밥먹자");
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("mentioned")
+    @Test
+    void mentioned() {
+        String expected = "정예지 님이 '밥먹자' 일정에서 당신을 멘션했어요.";
+        String actual = AlarmContent.MENTIONED.getMessage("정예지", "밥먹자");
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("schedule_update")
+    @Test
+    void schedule_update() {
+        String expected = "'밥먹자' 일정이 수정되었어요.";
+        String actual = AlarmContent.SCHEDULE_UPDATE.getMessage("밥먹자");
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("schedule_noti")
+    @Test
+    void schedule_noti() {
+        String expected = "'밥먹자' 일정이 한 시간 뒤에 예정되어 있어요.";
+        String actual = AlarmContent.SCHEDULE_NOTI.getMessage("밥먹자", "한 시간 뒤에");
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("파라미터가 1개인데 0개가 들어왔을 때")
+    @Test
+    void no_parameter() {
+        assertThatThrownBy(() -> AlarmContent.FRIEND_REQUEST.getMessage()).isInstanceOf(
+            MissingFormatArgumentException.class);
+    }
+
+    @DisplayName("파라미터가 2개인데 1개가 들어왔을 때")
+    @Test
+    void not_enough_parameter() {
+        assertThatThrownBy(() -> AlarmContent.MENTIONED.getMessage()).isInstanceOf(
+            MissingFormatArgumentException.class);
+    }
+}
