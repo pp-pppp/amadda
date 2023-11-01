e40ca957d5b534bbcdccdd53422771e2700a86a7
Sohee Kwon <khappy517@gmail.com>
2023-11-01 09:18:04 +0900

    docs: FAB stories docs 수정

--- frontend/_packages/@external-temporal/components/molecule/FAB/FAB.stories.ts
+++ frontend/_packages/@external-temporal/components/molecule/FAB/FAB.stories.ts
@@ -7,6 +7,12 @@ const meta = {
   component: FAB,
   parameters: {
     layout: 'centered',
+    componentSubtitle: 'Floating Action Button입니다.',
+    docs: {
+      description: {
+        component: '중요한 작업을 위한 디렉션을 제공하는 버튼입니다.',
+      },
+    },
   },
   tags: ['autodocs'],
   argTypes: {},
