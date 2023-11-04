eafade9cadfc707366a8c216db2d89db82692971
Jeong Yeji <jeongyeji.mail@gmail.com>
2023-11-04 19:53:22 +0900

    Feat: 일정 할당 알림 (#75)
    * feat: mock stubbing
    
    * feat: 일정 할당 알림
    
    * refactor: 중복 테스트 코드를 메소드로 추출
    
    * feat: 사용하지 않는 예외 코드 삭제

--- backend/src/main/java/com/pppppp/amadda/alarm/service/AlarmService.java
+++ backend/src/main/java/com/pppppp/amadda/alarm/service/AlarmService.java
@@ -2,14 +2,21 @@ package com.pppppp.amadda.alarm.service;
 
 import com.pppppp.amadda.alarm.dto.topic.alarm.AlarmFriendAccept;
 import com.pppppp.amadda.alarm.dto.topic.alarm.AlarmFriendRequest;
+import com.pppppp.amadda.alarm.dto.topic.alarm.AlarmScheduleAssigned;
 import com.pppppp.amadda.alarm.entity.KafkaTopic;
 import com.pppppp.amadda.friend.entity.FriendRequest;
 import com.pppppp.amadda.friend.repository.FriendRequestRepository;
 import com.pppppp.amadda.global.entity.exception.RestApiException;
 import com.pppppp.amadda.global.entity.exception.errorcode.FriendRequestErrorCode;
+import com.pppppp.amadda.global.entity.exception.errorcode.ScheduleErrorCode;
 import com.pppppp.amadda.global.entity.exception.errorcode.UserErrorCode;
+import com.pppppp.amadda.schedule.entity.Participation;
+import com.pppppp.amadda.schedule.entity.Schedule;
+import com.pppppp.amadda.schedule.repository.ParticipationRepository;
+import com.pppppp.amadda.schedule.repository.ScheduleRepository;
 import com.pppppp.amadda.user.entity.User;
 import com.pppppp.amadda.user.repository.UserRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,13 +28,15 @@ public class AlarmService {
 
     private final UserRepository userRepository;
     private final FriendRequestRepository friendRequestRepository;
+    private final ScheduleRepository scheduleRepository;
+    private final ParticipationRepository participationRepository;
     private final KafkaProducer kafkaProducer;
 
     @Transactional
-    public void sendFriendRequest(Long friendRequestSeq, Long ownerSeq, Long friendSeq) {
+    public void sendFriendRequest(Long ownerSeq, Long friendSeq) {
         User owner = getUser(ownerSeq);
         User friend = getUser(friendSeq);
-        FriendRequest friendRequest = getFriendRequest(friendRequestSeq, owner, friend);
+        FriendRequest friendRequest = getFriendRequest(owner, friend);
 
         AlarmFriendRequest value = AlarmFriendRequest.create(friendRequest.getRequestSeq(),
             owner.getUserSeq(), owner.getUserName());
@@ -46,21 +55,37 @@ public class AlarmService {
         kafkaProducer.sendAlarm(KafkaTopic.ALARM_FRIEND_ACCEPT, owner.getUserSeq(), value);
     }
 
+    @Transactional
+    public void sendScheduleAssigned(Long scheduleSeq) {
+        Schedule schedule = getSchedule(scheduleSeq);
+        User owner = schedule.getUser();
+
+        List<Participation> participations = participationRepository.findBySchedule_ScheduleSeqAndIsDeletedFalse(
+            scheduleSeq);
+        participations.stream().filter(participation -> !owner.equals(participation.getUser()))
+            .forEach(participation -> {
+                AlarmScheduleAssigned value = AlarmScheduleAssigned.create(
+                    schedule.getScheduleSeq(), participation.getScheduleName(),
+                    owner.getUserSeq(), owner.getUserName());
+                kafkaProducer.sendAlarm(KafkaTopic.ALARM_SCHEDULE_ASSIGNED,
+                    participation.getUser().getUserSeq(), value);
+            });
+    }
+
     private User getUser(Long ownerSeq) {
         return userRepository.findByUserSeq(ownerSeq)
             .orElseThrow(() -> new RestApiException(UserErrorCode.USER_NOT_FOUND));
     }
 
-    private FriendRequest getFriendRequest(Long friendRequestSeq, User owner, User friend) {
-        FriendRequest friendRequest = friendRequestRepository.findByOwnerAndFriend(owner, friend)
+    private Schedule getSchedule(Long scheduleSeq) {
+        return scheduleRepository.findByScheduleSeqAndIsDeletedFalse(scheduleSeq)
+            .orElseThrow(() -> new RestApiException(ScheduleErrorCode.SCHEDULE_NOT_FOUND));
+    }
+
+    private FriendRequest getFriendRequest(User owner, User friend) {
+        return friendRequestRepository.findByOwnerAndFriend(owner, friend)
             .orElseThrow(() -> new RestApiException(
                 FriendRequestErrorCode.FRIEND_REQUEST_NOT_FOUND));
-
-        if (!friendRequestSeq.equals(friendRequest.getRequestSeq())) {
-            throw new RestApiException(FriendRequestErrorCode.FRIEND_REQUEST_INVALID);
-        }
-
-        return friendRequest;
     }
 
 }
--- backend/src/test/java/com/pppppp/amadda/alarm/service/AlarmServiceTest.java
+++ backend/src/test/java/com/pppppp/amadda/alarm/service/AlarmServiceTest.java
@@ -10,9 +10,14 @@ import com.pppppp.amadda.alarm.dto.topic.BaseTopicValue;
 import com.pppppp.amadda.alarm.entity.KafkaTopic;
 import com.pppppp.amadda.friend.entity.FriendRequest;
 import com.pppppp.amadda.friend.repository.FriendRequestRepository;
+import com.pppppp.amadda.schedule.entity.Participation;
+import com.pppppp.amadda.schedule.entity.Schedule;
+import com.pppppp.amadda.schedule.repository.ParticipationRepository;
+import com.pppppp.amadda.schedule.repository.ScheduleRepository;
 import com.pppppp.amadda.user.entity.User;
 import com.pppppp.amadda.user.repository.UserRepository;
 import java.util.List;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -41,11 +46,19 @@ class AlarmServiceTest extends IntegrationTestSupport {
     @Autowired
     private FriendRequestRepository friendRequestRepository;
 
+    @Autowired
+    private ScheduleRepository scheduleRepository;
+
+    @Autowired
+    private ParticipationRepository participationRepository;
+
     @MockBean
     KafkaTemplate<Long, BaseTopicValue> kafkaTemplate;
 
     @AfterEach
     void tearDown() {
+        participationRepository.deleteAllInBatch();
+        scheduleRepository.deleteAllInBatch();
         friendRequestRepository.deleteAllInBatch();
         userRepository.deleteAllInBatch();
     }
@@ -54,22 +67,19 @@ class AlarmServiceTest extends IntegrationTestSupport {
     @Test
     void friend_request() {
         // given
-        User u1 = User.create(1111L, "유저1", "id1", "imageUrl1");
-        User u2 = User.create(1234L, "유저2", "id2", "imageUrl2");
-        List<User> users = userRepository.saveAll(List.of(u1, u2));
+        List<User> users = createUser();
         User owner = users.get(0);
         User friend = users.get(1);
 
         FriendRequest friendRequest = FriendRequest.create(owner, friend);
-        FriendRequest savedFriendRequest = friendRequestRepository.save(friendRequest);
+        friendRequestRepository.save(friendRequest);
 
         // when
-        alarmService.sendFriendRequest(savedFriendRequest.getRequestSeq(), owner.getUserSeq(),
-            friend.getUserSeq());
+        alarmService.sendFriendRequest(owner.getUserSeq(), friend.getUserSeq());
 
         // then
         String topic = KafkaTopic.ALARM_FRIEND_REQUEST;
-        Long key = 1234L;
+        Long key = friend.getUserSeq();
         verify(kafkaTemplate, times(1)).send(eq(topic), eq(key), any());
     }
 
@@ -77,21 +87,58 @@ class AlarmServiceTest extends IntegrationTestSupport {
     @Test
     void friend_accept() {
         // given
-        User u1 = User.create(1111L, "유저1", "id1", "imageUrl1");
-        User u2 = User.create(1234L, "유저2", "id2", "imageUrl2");
-        List<User> users = userRepository.saveAll(List.of(u1, u2));
+        List<User> users = createUser();
         User owner = users.get(0);
         User friend = users.get(1);
 
         FriendRequest friendRequest = FriendRequest.create(owner, friend);
-        FriendRequest savedFriendRequest = friendRequestRepository.save(friendRequest);
+        friendRequestRepository.save(friendRequest);
 
         // when
         alarmService.sendFriendAccept(owner.getUserSeq(), friend.getUserSeq());
 
         // then
         String topic = KafkaTopic.ALARM_FRIEND_ACCEPT;
-        Long key = 1111L;
+        Long key = owner.getUserSeq();
         verify(kafkaTemplate, times(1)).send(eq(topic), eq(key), any());
+    }
+
+    @DisplayName("일정 할당 알람")
+    @Test
+    void schedule_assigned() {
+        // given
+        List<User> users = createUser();
+        User user1 = users.get(0);
+        User user2 = users.get(1);
+
+        Schedule schedule = Schedule.builder().user(user1).build();
+        Schedule savedSchedule = scheduleRepository.save(schedule);
+
+        Participation participation1 = Participation.builder()
+            .user(user1)
+            .schedule(savedSchedule)
+            .scheduleName("밥")
+            .build();
+        Participation participation2 = Participation.builder()
+            .user(user2)
+            .schedule(savedSchedule)
+            .scheduleName("밥밥")
+            .build();
+        participationRepository.saveAll(List.of(participation1, participation2));
+
+        // when
+        alarmService.sendScheduleAssigned(savedSchedule.getScheduleSeq());
+
+        // then
+        String topic = KafkaTopic.ALARM_SCHEDULE_ASSIGNED;
+        Long key = user2.getUserSeq();
+        verify(kafkaTemplate, times(1)).send(eq(topic), eq(key), any());
+    }
+
+    @NotNull
+    private List<User> createUser() {
+        User u1 = User.create(1111L, "유저1", "id1", "imageUrl1");
+        User u2 = User.create(1234L, "유저2", "id2", "imageUrl2");
+        return userRepository.saveAll(List.of(u1, u2));
     }
 }
