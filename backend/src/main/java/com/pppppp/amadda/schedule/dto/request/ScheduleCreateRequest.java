1dc6cc4c63e4967627cb94b5e511813be46836f7
donggeoniiii <pkgun0727@gmail.com>
2023-11-06 17:47:16 +0900

    chore: 일정 생성 dto에 카테고리 정보 추가

--- backend/src/main/java/com/pppppp/amadda/schedule/dto/request/ScheduleCreateRequest.java
+++ backend/src/main/java/com/pppppp/amadda/schedule/dto/request/ScheduleCreateRequest.java
@@ -28,7 +28,9 @@ public record ScheduleCreateRequest(
 
     @NotNull(message = "알림 시간 설정이 필요해요!") AlarmTime alarmTime,
     @NotBlank(message = "일정 이름을 입력해주세요!") String scheduleName,
-    String scheduleMemo
+    String scheduleMemo,
+
+    Long categorySeq
 ) {
 
     public static ScheduleCreateRequest of(Schedule schedule,
@@ -46,6 +48,7 @@ public record ScheduleCreateRequest(
             .alarmTime(participation.getAlarmTime())
             .scheduleName(participation.getScheduleName())
             .scheduleMemo(participation.getScheduleMemo())
+            .categorySeq(participation.getCategory().getCategorySeq())
             .build();
     }
 
