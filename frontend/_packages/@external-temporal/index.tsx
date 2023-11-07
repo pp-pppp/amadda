dd59614d91035a915e196a97f9ef9453cfcc4fed
brewcoldblue <seung.yoon.yu@gmail.com>
2023-11-07 14:34:05 +0900

    chore: 컴포넌트를 외부 모듈에서 사용할 수 있도록 export

--- frontend/_packages/@external-temporal/index.tsx
+++ frontend/_packages/@external-temporal/index.tsx
@@ -20,6 +20,7 @@ export * from './components/interaction/FAB/FAB';
 export * from './components/view/Tooltip/Tooltip';
 export * from './components/view/Filter/Filter';
 export * from './components/view/Card/Card';
+export * from './components/view/List/List';
 
 export * from './components/layout/Flex';
 
