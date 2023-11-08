e41c0d7c258be4618ebc03199dc4abc4b2f5245b
Sohee Kwon <khappy517@gmail.com>
2023-11-08 18:43:16 +0900

    feat: 카카오 버튼 css 추가

--- frontend/_apps/shell/pages/index.tsx
+++ frontend/_apps/shell/pages/index.tsx
@@ -1,16 +1,23 @@
-import { IndexLayout } from '@/layout/IndexLayout';
 import { H1, Flex, Spacing } from 'external-temporal';
 import { signIn } from 'next-auth/react';
+import { IndexLayout } from '@/layout/IndexLayout';
+import { BASE } from '@/components/KakaoBtn/KakaoBtn.css';
+
+const kakaoBtnStyle = {
+  cursor: 'pointer',
+};
 
 export default function Page() {
   return (
     <IndexLayout>
       <Flex justifyContents="center" alignItems="center" flexDirection="column">
         <H1>Amadda</H1>
-        <Spacing size="5" />
+        <Spacing size="5" />{' '}
         <img
           src="image/kakao_login.png"
+          alt="카카오로 로그인하기"
           onClick={() => signIn('kakao', { callbackUrl: '/main' })}
+          className={BASE}
         />
       </Flex>
     </IndexLayout>
