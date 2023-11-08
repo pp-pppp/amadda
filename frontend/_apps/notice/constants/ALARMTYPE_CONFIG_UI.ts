ec5d4c4168a74bef28a32e39bd70c4521d899600
brewcoldblue <seung.yoon.yu@gmail.com>
2023-11-08 10:40:36 +0900

    feat: 알림 타입에 따른 UI 상수변수 추가

---
+++ frontend/_apps/notice/constants/ALARMTYPE_CONFIG_UI.ts
@@ -0,0 +1,8 @@
+export default {
+  FRIEND_REQUEST: '친구 신청',
+  FRIEND_ACCEPT: '친구 신청 수락',
+  SCHEDULE_ASSIGNED: '일정 할당',
+  MENTIONED: '일정 댓글 멘션',
+  SCHEDULE_UPDATE: '일정 업데이트',
+  SCHEDULE_NOTI: '일정 알림',
+};
