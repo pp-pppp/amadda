35ae2a19f2a6be17435bbdb4e5a633c43b299b8e
brewcoldblue <seung.yoon.yu@gmail.com>
2023-11-08 12:31:55 +0900

    chore: nextjs basepath 설정

--- frontend/_apps/notice/next.config.js
+++ frontend/_apps/notice/next.config.js
@@ -3,6 +3,7 @@ const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 const withVanillaExtract = createVanillaExtractPlugin();
 module.exports = withVanillaExtract({
   transpilePackages: ['ui'],
+  basePath: '/notice',
   webpack(config, options) {
     if (!options.isServer) {
       config.plugins.push(
