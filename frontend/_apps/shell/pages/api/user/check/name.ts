43a2c3028cfbb357184b8b017bba67200baa9494
brewcoldblue <seung.yoon.yu@gmail.com>
2023-11-10 13:09:17 +0900

    feat: shell api 구현

---
+++ frontend/_apps/shell/pages/api/user/check/name.ts
@@ -0,0 +1,22 @@
+import { http } from '@/utils/http';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import type { UserNameCheckResponse } from 'amadda-global-types';
+
+const name = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    //닉네임 유효성 검사
+    try {
+      const SPRING_RES = await http.post<UserNameCheckResponse>(
+        `${process.env.SPRING_API_ROOT}/user/check/name`,
+        req.body
+      );
+      res.status(SPRING_RES.status).json(SPRING_RES.data);
+    } catch (err) {
+      res
+        .status(err.status || 500)
+        .json(err?.data || { data: 'internal server error' });
+    }
+  }
+  res.status(400).json({ data: 'bad request' });
+};
+export default name;
---
+++ frontend/_apps/shell/pages/api/user/refresh.ts
@@ -0,0 +1,22 @@
+import { http } from '@/utils/http';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import type { UserJwtResponse } from 'amadda-global-types';
+
+const refresh = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    //Refresh 주고 새 토큰 세개 받기
+    try {
+      const SPRING_RES = await http.post<UserJwtResponse>(
+        `${process.env.SPRING_API_ROOT}/user/refresh`,
+        req.body
+      );
+      res.status(SPRING_RES.status).json(SPRING_RES.data);
+    } catch (err) {
+      res
+        .status(err.status || 500)
+        .json(err?.data || { data: 'internal server error' });
+    }
+  }
+  res.status(400).json({ data: 'bad request' });
+};
+export default refresh;
