a9acd4679c7b3cf0a486587cc0374c4854ba6729
Sohee Kwon <khappy517@gmail.com>
2023-11-10 10:25:38 +0900

    feat: mobile monthly plate 컴포넌트 추가

--- frontend/_apps/schedule/components/MobileMonthlyPlate/MobileMonthlyPlate.tsx
+++ frontend/_apps/schedule/components/MobileMonthlyPlate/MobileMonthlyPlate.tsx
@@ -1,12 +1,12 @@
 import { ReactNode } from 'react';
 import { Icon, Span } from '#/index';
-import { BASE, SELECTED } from './MobileMontlyPlate.css';
+import { BASE, SELECTED } from './MobileMonthlyPlate.css';
 
 export interface MobileMonthPlateProps {
   dateType: keyof typeof dateColor;
+  isScheduled?: boolean;
   isSelected?: boolean;
   children: string;
-  // children: ReactNode;
 }
 
 export interface dateColorValues {
@@ -20,17 +20,16 @@ const dateColor: dateColorValues = {
 
 export default function MobileMonthPlate({
   dateType,
+  isScheduled = false,
   isSelected = false,
   children,
 }: MobileMonthPlateProps) {
-  const rand = Math.floor(Math.random() * 31) + 1;
-  isSelected = children === rand.toString() ? true : false;
-  const className = isSelected ? `${BASE}` : `${BASE} ${SELECTED}`;
+  const className = isSelected ? `${BASE} ${SELECTED}` : `${BASE}`;
 
   return (
     <div className={className}>
       <Span color={dateColor[dateType]}>{children}</Span>
-      <Icon type="dot" color="darkgrey" />
+      {isScheduled && <Icon type="dot" color="darkgrey" />}
     </div>
   );
 }
