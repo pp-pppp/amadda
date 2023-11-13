b20a647b1915c40b27025d517aa8273ac872daf2
brewcoldblue <seung.yoon.yu@gmail.com>
2023-11-13 23:36:14 +0900

    chore: friend 관련 global types/interface 추가

--- frontend/_packages/global-types/api/FriendReq.d.ts
+++ frontend/_packages/global-types/api/FriendReq.d.ts
@@ -1,3 +1,14 @@
-export interface GroupCreateRequest {}
-export interface GroupPutRequest {}
-export interface FriendRequestRequest {}
+export interface GroupCreateRequest {
+  ownerSeq: number;
+  groupName: string;
+  userSeqs: Array<number>;
+}
+export interface GroupPutRequest {
+  ownerSeq: number;
+  groupName: string;
+  userSeqs: Array<number>;
+}
+export interface FriendRequestRequest {
+  userSeq: number; // 친구요청을 보내는 사람
+  targetSeq: number; // 친구요청을 보낸 상대
+}
--- frontend/_packages/global-types/api/FriendRes.d.ts
+++ frontend/_packages/global-types/api/FriendRes.d.ts
@@ -1,3 +1,13 @@
-export interface FriendRequestResponse {}
-export interface FriendReadResponse {}
-export interface GroupDeleteResponse {}
+export interface User {
+  userSeq: number;
+  userName: string;
+  userId: string;
+  imageUrl: string;
+}
+export interface Group {
+  groupSeq: number;
+  groupName: string;
+  groupMember: Array<User>;
+}
+
+export type FriendReadResponse = Array<Group>;
