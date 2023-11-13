ae01c59519831e3a76a8c93e4241648eeec42b5f
Sohee Kwon <khappy517@gmail.com>
2023-11-13 11:14:58 +0900

    chore: 각 _app.tsx에서 리덕스 wrapper 삭제

--- frontend/_apps/shell/pages/_app.tsx
+++ frontend/_apps/shell/pages/_app.tsx
@@ -3,10 +3,8 @@ import { lazy } from 'react';
 import { style } from 'external-temporal';
 import { AppLayout } from '@SH/layout/AppLayout';
 import { SessionProvider } from 'next-auth/react';
-import wrapper from '@SH/store/store';
 
 function App({ Component, pageProps: { session, ...pageProps } }) {
-  const { store, props } = wrapper.useWrappedStore(pageProps);
   return (
     <SessionProvider session={session}>
       <AppLayout>
--- frontend/_apps/user/pages/_app.tsx
+++ frontend/_apps/user/pages/_app.tsx
@@ -1,10 +1,8 @@
 // import dynamic from 'next/dynamic';
 import { lazy } from 'react';
 import { style } from 'external-temporal';
-import wrapper from '@U/store/store';
 
 function App({ Component, pageProps }) {
-  const { store, props } = wrapper.useWrappedStore(pageProps);
   return (
     <>
       {/* <Nav /> */}
