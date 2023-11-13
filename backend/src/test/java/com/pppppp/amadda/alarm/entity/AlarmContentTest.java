ae7f38d1f6ee9d98ca02d691ba306f12650d3c1d
jeong-yeji <jeongyeji.mail@gmail.com>
2023-11-13 16:02:12 +0900

    test: 테스트 환경 통합

--- backend/src/test/java/com/pppppp/amadda/alarm/entity/AlarmContentTest.java
+++ backend/src/test/java/com/pppppp/amadda/alarm/entity/AlarmContentTest.java
@@ -3,11 +3,12 @@ package com.pppppp.amadda.alarm.entity;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.pppppp.amadda.IntegrationTestSupport;
 import java.util.MissingFormatArgumentException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class AlarmContentTest {
+class AlarmContentTest extends IntegrationTestSupport {
 
     @DisplayName("friend_request")
     @Test
