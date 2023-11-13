fdd142a1610ff4c168da72e20cb3a12b23d6cf6e
brewcoldblue <seung.yoon.yu@gmail.com>
2023-11-14 00:49:55 +0900

    fix: onClick 함수를 제대로 받지 않던 것 수정

--- frontend/_packages/@external-temporal/components/interaction/BtnRound/BtnRound.tsx
+++ frontend/_packages/@external-temporal/components/interaction/BtnRound/BtnRound.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import type { HTMLAttributes } from 'react';
+import type { HTMLAttributes, MouseEvent } from 'react';
 import { SIZE, VARIANTS } from './BtnRound.css';
 
 export interface BtnRoundProps extends HTMLAttributes<HTMLButtonElement> {
@@ -7,7 +7,7 @@ export interface BtnRoundProps extends HTMLAttributes<HTMLButtonElement> {
   variant: keyof typeof VARIANTS;
   size?: keyof typeof SIZE;
   disabled: boolean;
-  onClick: () => void;
+  onClick?: (e: MouseEvent) => void;
   children: string;
 }
 export function BtnRound({
@@ -16,10 +16,16 @@ export function BtnRound({
   disabled,
   size = 'M',
   children,
+  onClick,
 }: BtnRoundProps) {
   const className = `${VARIANTS[variant]} ${SIZE[size]}`;
   return (
-    <button className={className} disabled={disabled} type={type}>
+    <button
+      className={className}
+      disabled={disabled}
+      type={type}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
