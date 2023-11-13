f654a058d0213ece432598dc26f32d2720b15295
donggeoniiii <pkgun0727@gmail.com>
2023-11-13 16:35:36 +0900

    comment: 주석 처리한 사용 하지 않는 메소드 삭제

--- backend/src/main/java/com/pppppp/amadda/schedule/dto/response/ScheduleListReadResponse.java
+++ backend/src/main/java/com/pppppp/amadda/schedule/dto/response/ScheduleListReadResponse.java
@@ -28,31 +28,6 @@ public record ScheduleListReadResponse(
     CategoryReadResponse category
 ) {
 
-/*    public static ScheduleListReadResponse of(Schedule schedule,
-        UserReadResponse authorizedUser, List<UserReadResponse> participants,
-        Participation participation) {
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
-        return ScheduleListReadResponse.builder()
-            .scheduleSeq(schedule.getScheduleSeq())
-            .scheduleName(participation.getScheduleName())
-            .participants(participants)
-            .isTimeSelected(schedule.isTimeSelected())
-            .isDateSelected(schedule.isDateSelected())
-            .isAllDay(schedule.isAllDay())
-            .scheduleStartAt(
-                (schedule.isDateSelected()) ? schedule.getScheduleStartAt().format(formatter) : "")
-            .scheduleEndAt(
-                (schedule.isTimeSelected()) ? schedule.getScheduleEndAt().format(formatter) : "")
-            .alarmTime(participation.getAlarmTime().getContent())
-            .isAuthorizedAll(schedule.isAuthorizedAll())
-            .authorizedUser(authorizedUser)
-            .category((participation.getCategory() != null) ?
-                CategoryReadResponse.of(participation.getCategory()) : null)
-            .build();
-    }*/
-
     public static ScheduleListReadResponse of(Schedule schedule,
         UserReadResponse authorizedUser, List<UserReadResponse> participants,
         Participation participation, Boolean isFinished) {
