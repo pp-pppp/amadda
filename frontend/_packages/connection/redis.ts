45e78c417ce0787a4ea7a5032c747d0d193b9c05
brewcoldblue <seung.yoon.yu@gmail.com>
2023-11-14 16:42:41 +0900

    feat: connection 패키지 추가

---
+++ frontend/_packages/connection/redis.ts
@@ -0,0 +1,30 @@
+import { createClient } from 'redis';
+export const REDIS = {
+  async getRefreshToken(k: string): Promise<string> {
+    const client = createClient({ url: `${process.env.REDIS_ROOT as string}` });
+    /*
+		{
+    username: 'default', // use your Redis user. More info https://redis.io/docs/management/security/acl/
+    password: 'secret', // use your password here
+    socket: {
+        host: 'my-redis.cloud.redislabs.com',
+        port: 6379,
+        tls: true,
+        key: readFileSync('./redis_user_private.key'),
+        cert: readFileSync('./redis_user.crt'),
+        ca: [readFileSync('./redis_ca.pem')]
+    }
+}
+		*/
+    await client.connect();
+    const token = (await client.get(k)) || '';
+    await client.disconnect();
+    return token;
+  },
+  async setRefreshToken(k: string, token: string) {
+    const client = createClient({ url: `${process.env.REDIS_ROOT as string}` });
+    await client.connect();
+    await client.set(k, token);
+    await client.disconnect();
+  },
+};
