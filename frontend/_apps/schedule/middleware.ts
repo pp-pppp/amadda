37ba107e1d28cbae20aed71c0839fb41c0e79ef9
brewcoldblue <seung.yoon.yu@gmail.com>
2023-11-16 11:12:34 +0900

    fix: matcher를 패키지에서 불러오지 않고 정적으로 수정

--- frontend/_apps/schedule/middleware.ts
+++ frontend/_apps/schedule/middleware.ts
@@ -1,7 +1,10 @@
-import { gateway, middlewareConfig } from 'connection';
+import { gateway } from 'connection';
 import { NextRequest } from 'next/server';
 
-export const config = middlewareConfig;
+export const config = {
+  matcher: ['/((?!api/auth|api/user|_next/static|_next/image|favicon.ico).*)'],
+};
+
 export function middleware(req: NextRequest) {
   return gateway(req);
 }
