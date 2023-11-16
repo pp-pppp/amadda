c594d9d6af386f3e4981fc03358b6cc125d06090
Jeong Yeji <jeongyeji.mail@gmail.com>
2023-11-16 15:21:59 +0900

    Fix: 알림 API 수정 (#196)

--- backend/src/main/java/com/pppppp/amadda/alarm/dto/response/AlarmReadResponse.java
+++ backend/src/main/java/com/pppppp/amadda/alarm/dto/response/AlarmReadResponse.java
@@ -2,6 +2,10 @@ package com.pppppp.amadda.alarm.dto.response;
 
 import com.pppppp.amadda.alarm.entity.Alarm;
 import com.pppppp.amadda.alarm.entity.AlarmType;
+import com.pppppp.amadda.alarm.entity.FriendRequestAlarm;
+import com.pppppp.amadda.alarm.entity.ScheduleAlarm;
+import com.pppppp.amadda.global.entity.exception.RestApiException;
+import com.pppppp.amadda.global.entity.exception.errorcode.CommonErrorCode;
 import lombok.Builder;
 
 @Builder
@@ -10,15 +14,28 @@ public record AlarmReadResponse(
     String content,
     boolean isRead,
     AlarmType alarmType,
+    Long relatedSeq,
     boolean isEnabled) {
 
     public static AlarmReadResponse of(Alarm alarm, boolean isEnabled) {
-        return AlarmReadResponse.builder()
+        AlarmReadResponseBuilder builder = AlarmReadResponse.builder()
             .alarmSeq(builder().alarmSeq)
             .content(alarm.getContent())
             .isRead(alarm.isRead())
             .alarmType(alarm.getAlarmType())
-            .isEnabled(isEnabled)
-            .build();
+            .isEnabled(isEnabled);
+
+        if (alarm instanceof ScheduleAlarm scheduleAlarm) {
+            return builder
+                .relatedSeq(scheduleAlarm.getSchedule().getScheduleSeq())
+                .build();
+        }
+        if (alarm instanceof FriendRequestAlarm friendRequestAlarm) {
+            return builder
+                .relatedSeq(friendRequestAlarm.getFriendRequest().getRequestSeq())
+                .build();
+        }
+
+        throw new RestApiException(CommonErrorCode.INVALID_PARAMETER);
     }
 }
