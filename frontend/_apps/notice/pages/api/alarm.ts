2d3af76e3c072e86ffff020551059c029dfcbe8d
brewcoldblue <seung.yoon.yu@gmail.com>
2023-11-16 14:32:52 +0900

    feat: 알림 GET 및 SSE API 추가

--- frontend/_apps/notice/pages/api/alarm.ts
+++ frontend/_apps/notice/pages/api/alarm.ts
@@ -8,14 +8,14 @@ const alarm = async (req: NextApiRequest, res: NextApiResponse) => {
       const SPRING_RES = await http.get<AlarmReadResponse>(
         `${process.env.SPRING_API_ROOT}/alarm`
       );
-      res.status(SPRING_RES.status).json(SPRING_RES.data);
+      return res.status(SPRING_RES.status).json(SPRING_RES.data);
     } catch (err) {
-      res
+      return res
         .status(err.status || 500)
         .json(err?.data || { data: 'internal server error' });
     }
   }
-  res.status(400).json({ data: 'bad request' });
+  return res.status(400).json({ data: 'bad request' });
 };
 
 export default alarm;
