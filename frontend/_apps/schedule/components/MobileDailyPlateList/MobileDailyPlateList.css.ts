f90741c7a9b3af01d177880e33cb47844fe79c96
Sohee Kwon <khappy517@gmail.com>
2023-11-17 15:21:22 +0900

    feat: daily plate list css 수정

--- frontend/_apps/schedule/components/MobileDailyPlateList/MobileDailyPlateList.css.ts
+++ frontend/_apps/schedule/components/MobileDailyPlateList/MobileDailyPlateList.css.ts
@@ -4,6 +4,5 @@ import { style } from '@vanilla-extract/css';
 export const BASE = style({
   width: '100vw',
   maxWidth: '430px',
-  borderTop: `1px solid ${colors.GREY_300} `,
   paddingTop: '1rem',
 });
