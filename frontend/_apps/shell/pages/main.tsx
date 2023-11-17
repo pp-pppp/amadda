2f61f7411a75396e46617c19c1d3ac9255b233c7
brewcoldblue <seung.yoon.yu@gmail.com>
2023-11-17 13:52:06 +0900

    fix: main 화면의 api 요청 삭제

--- frontend/_apps/shell/pages/main.tsx
+++ frontend/_apps/shell/pages/main.tsx
@@ -14,16 +14,16 @@ export default function Main() {
   const [isInited, setIsInited] = useState(false);
 
   const fetcher = async () => {
-    const res = await axios.get('/api/auth/init', {
-      headers: {
-        Authorization: `Bearer ${session && session.user.accessToken}`,
-      },
-    });
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_SHELL}/api/auth/init`,
+      {
+        headers: {
+          init: `${session && session.user.accessToken}`,
+        },
+      }
+    );
     return res;
   };
-
-  const { data, error } = useSWR('/api/auth/init', fetcher);
-  // if (error) router.push('/');
 
   useEffect(() => {
     session?.user.userName && setUsername(session?.user.userName);
@@ -34,7 +34,7 @@ export default function Main() {
     return (
       <>
         <P>접속 유저 : {username} </P>
-        <P>{data && data.status}</P>
+        {/* <P>{data ? data.status : 'ERR'}</P> */}
         <P color="key">Shell(3000)의 메인 화면입니다.</P>
         <P color="key">추후 다른 포트에서 컴포넌트를 임포트할 예정이에요.</P>
       </>
