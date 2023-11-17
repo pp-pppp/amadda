7616c703a439d5cef6678fafdee80501b034e12b
Sohee Kwon <khappy517@gmail.com>
2023-11-17 14:13:38 +0900

    feat: index css 수정

--- frontend/_apps/schedule/components/MobileCalendar/MobileCalendarIndex/MobileCalendarIndex.css.ts
+++ frontend/_apps/schedule/components/MobileCalendar/MobileCalendarIndex/MobileCalendarIndex.css.ts
@@ -2,8 +2,9 @@ import { style } from '@vanilla-extract/css';
 import colors from '#/constants/colors';
 
 export const BASE = style({
-  display: 'flex',
-  justifyContent: 'space-around',
-  alignItems: 'center',
-  width: '430px',
+  display: 'grid',
+  gridTemplateColumns: 'repeat(7, 1fr)',
+  placeItems: 'center',
+  // width: '100vw',
+  // maxWidth: '430px',
 });
