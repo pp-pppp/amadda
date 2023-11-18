8547bcb57a1a15c4dd1790c3b07fff1f7b90ff35
brewcoldblue <seung.yoon.yu@gmail.com>
2023-11-18 15:53:34 +0900

    feat: 페이지에 SEO 최적화를 위한 Head 컴포넌트를 삽입합니다.

--- frontend/_apps/shell/pages/schedule/index.tsx
+++
@@ -1,8 +0,0 @@
-import Layout from '@SH/components/Layout';
-
-export default function Page({ children }) {
-  return <></>;
-}
-Page.getLayout = function getLayout(page) {
-  return <Layout>{page}</Layout>;
-};
