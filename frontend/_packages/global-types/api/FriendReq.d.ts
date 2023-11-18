b705e587dab421a018b4eb9511a741c19461cfb5
Se Yu <82228797+brewcoldblue@users.noreply.github.com>
2023-11-18 13:10:08 +0900

    Fix: 알림 api interface 수정 (#205)

--- frontend/_packages/global-types/api/FriendReq.d.ts
+++ frontend/_packages/global-types/api/FriendReq.d.ts
@@ -1,14 +1,11 @@
 export interface GroupCreateRequest {
-  ownerSeq: number;
   groupName: string;
   userSeqs: Array<number>;
 }
-export interface GroupPutRequest {
-  ownerSeq: number;
+export interface GroupUpdateRequest {
   groupName: string;
   userSeqs: Array<number>;
 }
 export interface FriendRequestRequest {
-  userSeq: number; // 친구요청을 보내는 사람
-  targetSeq: number; // 친구요청을 보낸 상대
+  targetSeq: number;
 }
