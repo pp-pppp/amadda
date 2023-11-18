d75ef0c1dd3b8f892d45dd4bb2cae0d1b72e9976
brewcoldblue <seung.yoon.yu@gmail.com>
2023-11-18 15:16:53 +0900

    feat: Icon 컴포넌트에 알림이 왔을 때 표시될 아이콘 및 로딩 컴포넌트 추가

---
+++ frontend/_packages/@external-temporal/components/view/Loading/Loading.stories.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Loading } from './Loading';
+
+const meta = {
+  title: 'External-temporal/Views/Loading',
+  component: Loading,
+  parameters: {
+    componentSubtitle: '로딩 컴포넌트입니다.',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Loading>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {},
+};
---
+++ frontend/_packages/@external-temporal/components/view/Loading/Loading.tsx
@@ -0,0 +1,5 @@
+import { BASE } from './Loading.css';
+
+export function Loading() {
+  return <div className={BASE}>정보를 가져오고 있어요.</div>;
+}
