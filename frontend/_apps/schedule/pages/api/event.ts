46877927121a9f0da745ee8b372e9b687cb318c8
brewcoldblue <seung.yoon.yu@gmail.com>
2023-11-18 18:15:54 +0900

    feat: schedule kafka eventstream api 추가

---
+++ frontend/_apps/schedule/pages/api/event.ts
@@ -0,0 +1,25 @@
+import { KAFKA_SCHEDULE } from 'connection';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function event(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+
+  try {
+    const consumer = await KAFKA_SCHEDULE();
+
+    await consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        res.status(204);
+      },
+    });
+
+    req.on('close', () => {
+      console.log('Client disconnected');
+      return res.end();
+    });
+  } catch (err) {
+    return res.status(500).json({ data: 'internal server error' });
+  }
+}
