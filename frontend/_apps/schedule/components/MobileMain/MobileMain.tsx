d554222bf9862a922d23887831012032ecb5ddde
Sohee Kwon <khappy517@gmail.com>
2023-11-18 18:17:04 +0900

    feat: 상수변수 추가

--- frontend/_apps/schedule/components/MobileMain/MobileMain.tsx
+++ frontend/_apps/schedule/components/MobileMain/MobileMain.tsx
@@ -2,13 +2,14 @@ import { FABLAYOUT_MOBILE, INDEXLAYOUT_MOBILE } from '@SCH/layout/Layout.css';
 import { FAB, Segments } from 'external-temporal';
 import { MobileMonthlyPage } from './MobileMonthlyPage/MobileMonthlyPage';
 import { MobileUnscheduledPage } from './MobileUnscheduledPage/MobileUnscheduledPage';
+import CALENDAR from '@SCH/constants/CALENDAR';
 
 export function MobileMain() {
   return (
     <div className={INDEXLAYOUT_MOBILE}>
       <Segments
-        titles={['월간', '미정']}
-        pages={[MobileMonthlyPage(), MobileUnscheduledPage()]}
+        titles={[CALENDAR.TITLE_MONTHLY, CALENDAR.TITLE_UNSCHEUDLED]}
+        pages={[<MobileMonthlyPage />, <MobileUnscheduledPage />]}
       />
       <div className={FABLAYOUT_MOBILE}>
         <FAB />
---
+++ frontend/_apps/schedule/constants/CALENDAR.ts
@@ -0,0 +1,6 @@
+export default {
+  TITLE_MONTHLY: '월간',
+  TITLE_UNSCHEUDLED: '미정',
+  NO_PLAN: '일정이 없어요',
+  TODAY: '오늘',
+};
