04376a5dd8fe4f71e41094d2f39fe48699c68db7
brewcoldblue <seung.yoon.yu@gmail.com>
2023-11-19 13:53:37 +0900

    feat: Label 컴포넌트가 children으로 ReactNode를 받도록 수정

--- frontend/_packages/@external-temporal/components/interaction/Label/Label.tsx
+++ frontend/_packages/@external-temporal/components/interaction/Label/Label.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import type { HTMLAttributes } from 'react';
+import type { HTMLAttributes, ReactNode } from 'react';
 import { BASE } from './Label.css';
 
 export interface LabelProps extends HTMLAttributes<HTMLLabelElement> {
   htmlFor: string;
-  children: string;
+  children: ReactNode;
 }
 
 export function Label({ htmlFor, children }: LabelProps) {
