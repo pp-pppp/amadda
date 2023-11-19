fe011b7b001238e18a161df7554f6e20cd7c3c67
mindy0414 <122426499+mindy0414@users.noreply.github.com>
2023-11-20 00:06:24 +0900

    Refactor: 아이디랑 이름 유효성 검사 수정 (#222)

--- backend/src/main/java/com/pppppp/amadda/user/dto/request/UserIdCheckRequest.java
+++ backend/src/main/java/com/pppppp/amadda/user/dto/request/UserIdCheckRequest.java
@@ -1,11 +1,11 @@
 package com.pppppp.amadda.user.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record UserIdCheckRequest(
-    @NotBlank(message = "유저의 아이디를 입력해 주세요.") String userId
+    @NotNull(message = "유저의 아이디를 입력해 주세요.") String userId
 ) {
 
 }
--- backend/src/main/java/com/pppppp/amadda/user/dto/request/UserNameCheckRequest.java
+++ backend/src/main/java/com/pppppp/amadda/user/dto/request/UserNameCheckRequest.java
@@ -1,11 +1,11 @@
 package com.pppppp.amadda.user.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record UserNameCheckRequest(
-    @NotBlank(message = "유저의 이름을 입력해 주세요.") String userName
+    @NotNull(message = "유저의 이름을 입력해 주세요.") String userName
 ) {
 
 }
