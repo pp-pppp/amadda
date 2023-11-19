6326b906827026eaa5f93cbb0c729ec82d721e8c
brewcoldblue <seung.yoon.yu@gmail.com>
2023-11-19 13:53:48 +0900

    feat: Chip 컴포넌트 추가

---
+++ frontend/_packages/@external-temporal/components/interaction/Chip/Chip.css.ts
@@ -0,0 +1,61 @@
+import colors from '#/constants/colors';
+import { style, styleVariants } from '@vanilla-extract/css';
+
+const BASE = style({
+  borderRadius: '2rem',
+  width: 'fit-content',
+  height: '1.75rem',
+  display: 'inline-flex',
+  alignItems: 'center',
+  fontSize: '0.8rem',
+  lineHeight: 0,
+  padding: '0 0.75rem 0 0.75rem',
+  marginBottom: '0.125rem',
+  marginRight: '0.25rem',
+  fontWeight: 400,
+});
+
+export const TYPE_VARIANT = styleVariants({
+  filter: [
+    BASE,
+    {
+      border: `1px solid ${colors.BLACK}`,
+      backgroundColor: colors.white,
+      color: colors.black,
+      cursor: 'pointer',
+    },
+  ],
+  filterselected: [
+    BASE,
+    { backgroundColor: colors.key, color: colors.WHITE, cursor: 'pointer' },
+  ],
+  keyword: [
+    BASE,
+    {
+      color: colors.white,
+      backgroundColor: colors.key,
+      paddingRight: '0.4rem',
+    },
+  ],
+  suggestion: [
+    BASE,
+    {
+      backgroundColor: colors.GREY_800,
+      color: colors.white,
+      cursor: 'pointer',
+    },
+  ],
+});
+
+export const SHAPE_VARIANT = styleVariants({
+  round: { borderRadius: 100 },
+  square: { borderRadius: '0.5rem' },
+});
+
+export const KEYWORD_DELETE = style({
+  color: colors.WHITE,
+  margin: 0,
+  padding: 0,
+  background: 'transparent',
+  border: 'transparent',
+});
