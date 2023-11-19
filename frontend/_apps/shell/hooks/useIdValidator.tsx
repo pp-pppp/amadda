41c00920b26317492ed7679ba7030eab2586d2c8
Sohee Kwon <khappy517@gmail.com>
2023-11-20 03:51:31 +0900

    feat: useIdValidator 수정

--- frontend/_apps/shell/hooks/useIdValidator.tsx
+++ frontend/_apps/shell/hooks/useIdValidator.tsx
@@ -3,38 +3,35 @@ import { UserIdCheckRequest, UserIdCheckResponse } from 'amadda-global-types';
 import { useState, useEffect } from 'react';
 
 export default function useIdValidator(id: string) {
-  const [userId, setUserId] = useState(id);
+  const [userId, setUserId] = useState<string>(id);
   const [isIdEmpty, setIsIdEmpty] = useState(false);
   const [isIdValid, setIsIdValid] = useState(true);
   const [isIdDuplicated, setIsIdDuplicated] = useState(false);
-  const [idValue, setIdValue] = useState('');
+  const [idValue, setIdValue] = useState(id);
   const [initial, setInitial] = useState(true);
-
   useEffect(() => {
-    const IdCheck = async () => {
-      const UserIdCheckRequestBody: UserIdCheckRequest = {
-        userId: idValue,
-      };
-
-      await http
-        .post<UserIdCheckRequest, UserIdCheckResponse>(
-          `${process.env.NEXT_PUBLIC_SHELL}/api/user/check/id`,
-          UserIdCheckRequestBody
-        )
-        .then(res => res.data)
-        .then(data => {
-          data.isDuplicated
-            ? setIsIdDuplicated(true)
-            : setIsIdDuplicated(false);
-          !data.isValid && setIdValue(idValue.slice(0, -1));
-        });
+    const UserIdCheckRequestBody: UserIdCheckRequest = {
+      userId: idValue,
     };
 
-    IdCheck();
-
-    initial && setInitial(false);
-    !initial && idValue.length === 0 ? setIsIdEmpty(true) : setIsIdEmpty(false);
-    idValue.length > 20 && setIdValue(idValue.slice(0, -1));
+    fetch(`${process.env.NEXT_PUBLIC_SHELL}/api/user/check/id`, {
+      method: 'POST',
+      body: JSON.stringify(UserIdCheckRequestBody),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => res.json())
+      .then(json => {
+        json.data.isDuplicated
+          ? setIsIdDuplicated(true)
+          : setIsIdDuplicated(false);
+        initial && setInitial(false);
+        !initial && idValue.length === 0
+          ? setIsIdEmpty(true)
+          : setIsIdEmpty(false);
+        idValue.length > 20 && setIdValue(idValue.slice(0, -1));
+      });
 
     setUserId(idValue);
   }, [idValue]);
@@ -46,5 +43,6 @@ export default function useIdValidator(id: string) {
     isIdDuplicated: isIdDuplicated,
     isIdValid: isIdValid,
     isIdEmpty: isIdEmpty,
+    setIsIdEmpty,
   };
 }
