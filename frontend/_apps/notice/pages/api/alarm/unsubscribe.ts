f3c912393d6a43f2a31df3828992daaf29784f36
brewcoldblue <seung.yoon.yu@gmail.com>
2023-11-20 04:02:00 +0900

    fix: fix typo

--- frontend/_apps/notice/pages/api/alarm/unsubscribe.ts
+++ frontend/_apps/notice/pages/api/alarm/unsubscribe.ts
@@ -5,7 +5,7 @@ const unsubscribe = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     if (req.method === 'POST') {
       const SPRING_RES = await http.post(
-        `${process.env.SPRING_API_ROOT}/alarm/subscribe`,
+        `${process.env.SPRING_API_ROOT}/alarm/unsubscribe`,
         req.body
       );
       res.status(SPRING_RES.status).json(SPRING_RES.data);
