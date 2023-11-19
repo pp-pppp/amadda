d5b5d066dd36ac1e0789a1185dc5afa6451f2769
Sohee Kwon <khappy517@gmail.com>
2023-11-20 03:51:13 +0900

    feat: useNameValidator 주석처리

--- frontend/_apps/shell/hooks/useNameValidator.tsx
+++ frontend/_apps/shell/hooks/useNameValidator.tsx
@@ -1,54 +1,56 @@
-import { http } from '@SH/utils/http';
-import {
-  UserNameCheckRequest,
-  UserNameCheckResponse,
-} from 'amadda-global-types';
-import { useState, useEffect } from 'react';
+// // import { http } from '@SH/utils/http';
+// import {
+//   UserNameCheckRequest,
+//   UserNameCheckResponse,
+// } from 'amadda-global-types';
+// import { http, clientHttp } from 'connection';
+// import { useState, useEffect } from 'react';
 
-export interface NameCheckRequest {
-  userName: string;
-}
+// export interface NameCheckRequest {
+//   userName: string;
+// }
 
-export interface NameCheckResponse {
-  isValid: boolean;
-}
+// export interface NameCheckResponse {
+//   isValid: boolean;
+// }
 
-export default function useNameValidator(name: string) {
-  const [nickname, setNickname] = useState<string>(name);
-  const [isNameValid, setIsNameValid] = useState(true);
-  const [isNameEmpty, setIsNameEmpty] = useState(false);
-  const [nameValue, setNameValue] = useState(name);
+// export default function useNameValidator(name: string) {
+//   const [nickname, setNickname] = useState<string>(name);
+//   const [isNameValid, setIsNameValid] = useState(true);
+//   const [isNameEmpty, setIsNameEmpty] = useState(false);
+//   const [nameValue, setNameValue] = useState(name);
 
-  useEffect(() => {
-    const NameCheck = async () => {
-      const UserNameCheckRequestBody: UserNameCheckRequest = {
-        userName: nameValue,
-      };
+//   useEffect(() => {
+//     // const NameCheck = () => {
+//     const UserNameCheckRequestBody: UserNameCheckRequest = {
+//       userName: nameValue,
+//     };
 
-      await http
-        .post<UserNameCheckRequest, UserNameCheckResponse>(
-          `${process.env.NEXT_PUBLIC_SHELL}/api/user/check/name`,
-          UserNameCheckRequestBody
-        )
-        .then(res => res.data)
-        .then(data => {
-          !data.isValid && setNameValue(nickname.slice(0, -1));
-        });
-    };
+//     fetch(`${process.env.NEXT_PUBLIC_SHELL}/api/user/check/name`, {
+//       method: 'POST',
+//       body: JSON.stringify(UserNameCheckRequestBody),
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//     })
+//       .then(res => res.json())
+//       .then(json => {
+//         setIsNameValid(json.data.isValid);
+//         return isNameValid;
+//       })
+//       .then(isValid => {});
+//     nameValue.length === 0 ? setIsNameEmpty(true) : setIsNameEmpty(false);
+//     nameValue.length > 20 && setNameValue(nameValue.slice(0, -1));
+//     !isNameValid && setNameValue(nameValue.slice(0, -1));
+//     setNickname(nameValue);
+//   }, [nameValue]);
 
-    NameCheck();
-    nameValue.length === 0 ? setIsNameEmpty(true) : setIsNameEmpty(false);
-    nameValue.length > 20 && setNameValue(nameValue.slice(0, -1));
-
-    setNickname(nameValue);
-  }, [nameValue]);
-
-  return {
-    nickname,
-    setNickname,
-    nameValue,
-    setNameValue,
-    isNameEmpty: isNameEmpty,
-    isNameValid: isNameValid,
-  };
-}
+//   return {
+//     nickname,
+//     setNickname,
+//     nameValue,
+//     setNameValue,
+//     isNameEmpty: isNameEmpty,
+//     isNameValid: isNameValid,
+//   };
+// }
