d57b2e7af796c12d047a46ef8cf8ea9ecb9135a8
brewcoldblue <seung.yoon.yu@gmail.com>
2023-11-19 17:10:19 +0900

    fix: edge runtime 문제로 kafka 모듈 분리

---
+++ frontend/_packages/amadda-kafka/kafka_notice.ts
@@ -0,0 +1,33 @@
+import { Kafka } from 'kafkajs';
+
+const groupId = process.env.KAFKA_CLIENT_GROUP_ID as string;
+const friendRequest = 'prod.alarm.friend-request';
+const friendAccept = 'prod.alarm.friend-accept';
+const scheduleAssigned = 'prod.alarm.schedule-assigned';
+const scheduleUpdate = 'prod.alarm.schedule-update';
+const scheduleNotification = 'prod.alarm.schedule-notification';
+const kafka = new Kafka({
+  clientId: `${process.env.KAFKA_CLIENT_ID}notice`,
+  brokers: [
+    process.env.KAFKA_BROKER_1,
+    process.env.KAFKA_BROKER_2,
+    process.env.KAFKA_BROKER_3,
+  ] as string[],
+});
+
+const notice_consumer = kafka.consumer({ groupId: groupId });
+export const KAFKA_NOTICE = async () => {
+  console.log('kafka-start subscribe');
+
+  await notice_consumer.connect();
+  await notice_consumer.subscribe({
+    topics: [
+      friendRequest,
+      friendAccept,
+      scheduleAssigned,
+      scheduleUpdate,
+      scheduleNotification,
+    ],
+  });
+  return notice_consumer;
+};
