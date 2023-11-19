c12c202032c9a2426086664794700c151309940d
brewcoldblue <seung.yoon.yu@gmail.com>
2023-11-20 03:43:31 +0900

    fix: expose 한 module type 지정

--- frontend/_packages/global-types/mf/mf-types.d.ts
+++ frontend/_packages/global-types/mf/mf-types.d.ts
@@ -6,3 +6,7 @@ declare module 'schedule/Calendar' {
   const Calendar: React.LazyExoticComponent<React.FC>;
   export default Calendar;
 }
+declare module 'schedule/Edit' {
+  const Edit: React.LazyExoticComponent<React.FC>;
+  export default Edit;
+}
