5fcf50a5d2788d9a9baeb2a74c9c80b9a893c2d2
brewcoldblue <82228797+brewcoldblue@users.noreply.github.com>
2023-11-19 21:31:56 +0900

    Fix: onFiltered와 onDelete가 모든 데이터를 넘길 수 있도록 수정 (#220)

--- frontend/_packages/@external-temporal/components/interaction/Chip/Chip.tsx
+++ frontend/_packages/@external-temporal/components/interaction/Chip/Chip.tsx
@@ -6,8 +6,8 @@ interface Props {
   type: keyof typeof TYPE_VARIANT;
   label: string;
   shape?: keyof typeof SHAPE_VARIANT;
-  onFiltered?: (label: string) => void;
-  onDelete?: (label: string) => void;
+  onFiltered?: (data: any) => void;
+  onDelete?: (data: any) => void;
 }
 
 export function Chip({
@@ -21,19 +21,13 @@ export function Chip({
   switch (type) {
     case 'filter':
       return (
-        <span
-          className={classname}
-          onClick={() => onFiltered && onFiltered(label)}
-        >
+        <span className={classname} onClick={onFiltered}>
           {label}
         </span>
       );
     case 'filterselected': //필터처럼 작동할 때에는 칩을 누르면 값이 상위 컴포넌트에 전달되어야 합니다. 따라서 onFlitered에 setter를 전달하면 됩니다.
       return (
-        <span
-          className={classname}
-          onClick={() => onFiltered && onFiltered(label)}
-        >
+        <span className={classname} onClick={onFiltered}>
           {label}
         </span>
       );
@@ -41,10 +35,7 @@ export function Chip({
       return (
         <span className={classname}>
           {label}
-          <button
-            className={KEYWORD_DELETE}
-            onClick={() => onDelete && onDelete(label)}
-          >
+          <button className={KEYWORD_DELETE} onClick={onDelete}>
             <Icon type="close" color="white" cursor="pointer" />
           </button>
         </span>
