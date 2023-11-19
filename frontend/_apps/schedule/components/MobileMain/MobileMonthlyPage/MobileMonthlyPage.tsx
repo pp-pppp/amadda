1b6afd3965dd499a91daa99ed7039fbc7f57d139
Sohee Kwon <khappy517@gmail.com>
2023-11-19 17:29:35 +0900

    fix: useEffect 의존성 배열 변경

--- frontend/_apps/schedule/components/MobileMain/MobileMonthlyPage/MobileMonthlyPage.tsx
+++ frontend/_apps/schedule/components/MobileMain/MobileMonthlyPage/MobileMonthlyPage.tsx
@@ -24,7 +24,7 @@ export function MobileMonthlyPage(): ReactNode {
     return () => {
       document.removeEventListener('mousedown', filterClose);
     };
-  }, [ref]);
+  }, []);
 
   return (
     <div ref={ref}>
