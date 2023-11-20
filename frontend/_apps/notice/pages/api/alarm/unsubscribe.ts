d6dd3929108340f3eb3cbc75b9c8e2a520b52058
brewcoldblue <82228797+brewcoldblue@users.noreply.github.com>
2023-11-20 13:29:52 +0900

    Feat: 알림 구독 및 구독 취소 브릿지 구현 (#224)

--- frontend/_apps/notice/pages/api/alarm/unsubscribe.ts
+++ frontend/_apps/notice/pages/api/alarm/unsubscribe.ts
@@ -1,10 +1,21 @@
+import { http } from 'connection';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 const unsubscribe = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'POST') {
-    try {
-    } catch (err) {}
+  try {
+    if (req.method === 'POST') {
+      const SPRING_RES = await http.post(
+        `${process.env.SPRING_API_ROOT}/alarm/unsubscribe`,
+        req.body
+      );
+      res.status(SPRING_RES.status).json(SPRING_RES.data);
+    }
+  } catch (err) {
+    res
+      .status(err.status || 500)
+      .json(err?.data || { data: 'internal server error' });
   }
+
   res.status(400).json({ data: 'bad request' });
 };
 
