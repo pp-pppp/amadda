7eb7fcfa013499872bfbae80822af5df2d00bf93
brewcoldblue <seung.yoon.yu@gmail.com>
2023-11-23 09:59:18 +0900

    fix: signup 로직 백엔드 구현에 맞추어 수정

--- frontend/_apps/shell/pages/api/user/signup.ts
+++ frontend/_apps/shell/pages/api/user/signup.ts
@@ -12,6 +12,7 @@ const signup = async (req: NextApiRequest, res: NextApiResponse) => {
       );
       res.status(SPRING_RES.status).json(SPRING_RES.data);
     } catch (err) {
+      console.log(err);
       res
         .status(err.status || 500)
         .json(err?.data || { data: 'internal server error' });
