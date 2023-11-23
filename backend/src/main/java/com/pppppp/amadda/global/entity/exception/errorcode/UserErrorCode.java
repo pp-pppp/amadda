9b167b7a1b39626a9651f79b16b2c2f3651c3392
mindy0414 <mmminjung0414@gmail.com>
2023-11-23 10:00:59 +0900

    refactor: 유저 아이디 이름 유효성 검사 수정

--- backend/src/main/java/com/pppppp/amadda/global/entity/exception/errorcode/UserErrorCode.java
+++ backend/src/main/java/com/pppppp/amadda/global/entity/exception/errorcode/UserErrorCode.java
@@ -10,6 +10,8 @@ public enum UserErrorCode implements ErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 유저가 존재하지 않습니다. "),
     USER_ID_DUPLICATED(HttpStatus.BAD_REQUEST, "해당 유저 id는 중복되는 값 입니다. "),
     USER_KAKAO_ID_DUPLICATED(HttpStatus.BAD_REQUEST, "해당 카카오 id는 이미 존재하는 아이디 입니다. "),
+    USER_ID_INVALID(HttpStatus.BAD_REQUEST, "해당 유저 id는 유효하지 않습니다. "),
+    USER_NAME_INVALID(HttpStatus.BAD_REQUEST, "해당 유저 이름은 유효하지 않습니다. "),
     ;
 
     private final HttpStatus httpStatus;
