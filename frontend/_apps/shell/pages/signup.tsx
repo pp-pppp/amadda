d8b51cca13ab1560415ed3be5b93b83196e77737
brewcoldblue <seung.yoon.yu@gmail.com>
2023-11-23 09:57:42 +0900

    feat: oAuth 인증로직 구현 (nextAuth -> 직접 구현)

---
+++ frontend/_apps/shell/pages/signup.tsx
@@ -0,0 +1,90 @@
+import { KV } from 'connection';
+import cookie from 'cookie';
+import type { GetServerSideProps } from 'next';
+import { UserJwtRequest, UserJwtResponse } from 'amadda-global-types';
+import SignUp from '@SH/components/SignUp/SignUp';
+interface KakaoPageProps {
+  kakaoId: string;
+  imageUrl: string;
+  userName: string;
+  isInited: boolean;
+}
+export default function KakaoPage({ ...props }: KakaoPageProps) {
+  return <SignUp userInfo={{ ...props }} />;
+}
+
+export const getServerSideProps = (async context => {
+  const { code } = context.query;
+  const TOKEN_RES = await fetch(
+    `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${process.env.NEXT_PUBLIC_KAKAO_KEY}&redirect_url=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&code=${code}`,
+    {
+      method: 'POST',
+      body: JSON.stringify(null),
+      headers: {
+        'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+      },
+    }
+  )
+    .then(res => res.json())
+    .catch(err => console.log(err));
+
+  const { access_token } = TOKEN_RES;
+  const USER_RES = await fetch(`https://kapi.kakao.com/v2/user/me`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+      'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+    },
+  })
+    .then(res => res.json())
+    .catch(err => console.log(err));
+
+  const UserJwtRequest: UserJwtRequest = {
+    kakaoId: USER_RES.id,
+    imageUrl: USER_RES.properties.profile_image,
+  };
+
+  const INIT: UserJwtResponse = await fetch(
+    `${process.env.SPRING_API_ROOT}/user/login`,
+    {
+      method: 'POST',
+      body: JSON.stringify(UserJwtRequest),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  )
+    .then(res => res.json())
+    .then(json => json.data)
+    .catch(err => console.log(err));
+
+  const { res } = context;
+  const COOKIE = cookie.serialize('Auth', INIT.accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== 'development',
+    path: '/',
+    sameSite: 'lax',
+    maxAge: 60 * 15,
+  });
+
+  res.setHeader('Set-Cookie', COOKIE);
+  await KV.setRefreshToken(INIT.refreshAccessKey, INIT.refreshToken);
+
+  const userdata: KakaoPageProps = {
+    kakaoId: USER_RES.id,
+    imageUrl: USER_RES.properties.profile_image,
+    userName: USER_RES.properties.nickname,
+    isInited: INIT.isInited,
+  };
+
+  if (userdata.isInited)
+    //이미 회원인 경우 리다이렉트합니다
+    return {
+      redirect: {
+        destination: `${process.env.NEXT_PUBLIC_SHELL}/schedule`,
+        permanent: false,
+      },
+    };
+
+  return { props: userdata };
+}) satisfies GetServerSideProps<KakaoPageProps>;
