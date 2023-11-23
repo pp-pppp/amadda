a50a4125f9e668b0a3aa5505b315dc51998070f5
brewcoldblue <seung.yoon.yu@gmail.com>
2023-11-23 09:56:07 +0900

    chore: next.config.js image domain option 수정

--- frontend/_apps/notice/next.config.js
+++ frontend/_apps/notice/next.config.js
@@ -2,7 +2,9 @@ const NextFederationPlugin = require('@module-federation/nextjs-mf');
 const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 const withVanillaExtract = createVanillaExtractPlugin();
 module.exports = withVanillaExtract({
-  domains: [process.env.KAKAO_CDN_DOMAIN, process.env.S3_DOMAIN],
+  images: {
+    domains: [process.env.KAKAO_CDN_DOMAIN, process.env.S3_DOMAIN],
+  },
   transpilePackages: ['ui'],
   basePath: '/mf/notice',
   async headers() {
