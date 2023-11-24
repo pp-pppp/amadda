35c2803044bf02feef8a782582a1b679d3f5f484
brewcoldblue <82228797+brewcoldblue@users.noreply.github.com>
2023-11-24 13:44:01 +0900

    Fix: middleware에서 서버 간 통신 시 쿠키를 명시적으로 첨부 (#243)

--- frontend/_packages/connection/middleware.ts
+++ frontend/_packages/connection/middleware.ts
@@ -23,7 +23,11 @@ export const gateway = async (req: NextRequest) => {
   const preflight: UserAccessResponse = await fetch(
     `${process.env.SPRING_API_ROOT as string}/user/access`,
     {
-      headers: { Cookie: `Auth=${AT}` }, //프리플라이트 요청을 보냅니다. 본 요청을 보내기 전에 헤더만 떼어 미리 보내고, 토큰이 어떤 상태인지 백엔드에 확인받습니다.
+      headers: {
+        'Content-Type': 'application/json',
+        Cookie: `Auth=${AT}`,
+      }, //프리플라이트 요청을 보냅니다. 본 요청을 보내기 전에 헤더만 떼어 미리 보내고, 토큰이 어떤 상태인지 백엔드에 확인받습니다.
+      credentials: 'include',
     }
   )
     .then(res => res.json())
@@ -32,7 +36,7 @@ export const gateway = async (req: NextRequest) => {
       return err.status;
     });
 
-  if ((await preflight.isBroken) || !preflight)
+  if (preflight.isBroken || !preflight)
     //토큰이 만료된 것이 아니라 손상되었다면 로그아웃시킵니다.
     return NextResponse.rewrite(
       `${process.env.NEXT_PUBLIC_SHELL}/api/auth/signout`
@@ -45,7 +49,11 @@ export const gateway = async (req: NextRequest) => {
       const re_preflight: UserJwtResponse = await fetch(
         `${process.env.SPRING_API_ROOT as string}/user/refresh`,
         {
-          headers: { Cookie: `Auth=${RT}` }, //Auth 헤더에 RT를 넣어서 백엔드에 확인받습니다.
+          headers: {
+            'Content-Type': 'application/json',
+            Cookie: `Auth=${RT}`,
+          }, //리프레시 토큰으로 프리플라이트 요청을 다시 보냅니다.
+          credentials: 'include',
         }
       ).then(res => res.json());
       //프리플라이트 응답이 잘 왔다면
@@ -58,10 +66,8 @@ export const gateway = async (req: NextRequest) => {
       const new_AT = re_preflight.accessToken; //새로운 액세스 토큰을 넣어서
       const response = NextResponse.next();
       response.cookies.set('Auth', new_AT, {
-        httpOnly: true,
         sameSite: 'lax',
         maxAge: 60 * 15,
-        secure: process.env.NODE_ENV !== 'development',
         path: '/',
       });
 
@@ -77,6 +83,10 @@ export const gateway = async (req: NextRequest) => {
   }
   //만약 처음에 받았던 액세스 토큰에 문제가 없었다면 요청을 그대로 통과시킵니다.
   const response = NextResponse.next();
-  return response;
+  response.cookies.set('Auth', AT, {
+    sameSite: 'lax',
+    maxAge: 60 * 15,
+    path: '/',
+  });
 };
 export default gateway;
