90b5739f4453c813fefce57a79ba2c27665129c5
brewcoldblue <seung.yoon.yu@gmail.com>
2023-11-29 11:34:52 +0900

    feat: kakaoAuth에 쓰는 메서드 작성

---
+++ frontend/_packages/connection/kakaoAuth.ts
@@ -0,0 +1,59 @@
+import { ApiResponse } from 'amadda-global-types';
+
+export default {
+  Get: async (url: string) => {
+    const result = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+      },
+    });
+    const response = await result.json();
+    return response;
+  },
+  post: async (url: string, body?: any) => {
+    const result = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(body) || null,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+      },
+    });
+    const response = await result.json();
+    return response;
+  },
+  secureGet: async (url: string, token: string) => {
+    const result = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+      },
+    });
+    const response = await result.json();
+    return response;
+  },
+  securepost: async (url: string, token: string, body?: any) => {
+    const result = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(body) || null,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+      },
+    });
+    const response = await result.json();
+    return response;
+  },
+  secureDelete: async (url: string, token: string) => {
+    const result = await fetch(url, {
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+      },
+    });
+    const response = await result.json();
+    return response;
+  },
+};
