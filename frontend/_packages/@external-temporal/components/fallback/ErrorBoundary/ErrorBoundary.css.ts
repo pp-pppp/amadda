7b6c737314a4784186f52e4a661a938fc4a61a06
brewcoldblue <seung.yoon.yu@gmail.com>
2023-11-29 14:34:04 +0900

    feat: ErrorBoundary 컴포넌트 추가

---
+++ frontend/_packages/@external-temporal/components/fallback/ErrorBoundary/ErrorBoundary.css.ts
@@ -0,0 +1,6 @@
+import { style } from '@vanilla-extract/css';
+
+export const BASE = style({
+  textAlign: 'center',
+  margin: '5rem 0 5rem 0',
+});
---
+++ frontend/_packages/@external-temporal/components/fallback/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Component, type ErrorInfo, type ReactNode } from 'react';
+import { BASE } from './ErrorBoundary.css';
+import { H2 } from '#/components/typography/Hn/H2';
+import { H3 } from '#/components/typography/Hn/H3';
+export interface ErrorBoundaryProps {
+  children?: ReactNode;
+}
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+export default class ErrorBoundary extends Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true };
+  }
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    // You can use your own error logging service here
+    console.log({ error, errorInfo });
+  }
+  render() {
+    // Check if the error is thrown
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return (
+        <div className={BASE}>
+          <H3>죄송해요! 잠시 후 다시 시도해주세요.</H3>
+        </div>
+      );
+    }
+
+    // Return children components in case of no error
+
+    return this.props.children;
+  }
+}
