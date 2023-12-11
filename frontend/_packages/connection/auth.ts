96452b760f754a0a9fcd89be17e9b855dcae0824
brewcoldblue <seung.yoon.yu@gmail.com>
2023-12-11 21:03:01 +0900

    fix: typo 수정

--- frontend/_packages/connection/auth.ts
+++ frontend/_packages/connection/auth.ts
@@ -1,6 +1,6 @@
 import { UserAccessResponse, UserJwtResponse } from 'amadda-global-types';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import * from '@sentry/nextjs';
+import * as Sentry from '@sentry/nextjs';
 import { KV } from './kv';
 
 type API_HANDLER = (request: NextApiRequest, response: NextApiResponse) => Promise<NextApiResponse | void>;
@@ -19,7 +19,7 @@ export function auth(fn: API_HANDLER): API_HANDLER {
       return res.setHeader('Set-Cookie', `Auth=${ACCESS_TOKEN}; Max-Age=900; HttpOnly; SameSite=Lax;`);
     } catch (err) {
       //모든 종류의 에러는 로그아웃으로 리다이렉트시킵니다.
-      Sentry
+      Sentry.captureException(err);
       return res.redirect(`${process.env.NEXT_PUBLIC_SHELL}/signout`);
     }
   };
