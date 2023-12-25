92db8e30e43f3a878be3bb9c7e2139d632cab1ce
brewcoldblue <seung.yoon.yu@gmail.com>
2023-12-25 13:30:25 +0900

    refactor: 에러 처리가 보다 명확히 되도록 수정

--- frontend/_packages/connection/http.ts
+++ frontend/_packages/connection/http.ts
@@ -2,80 +2,80 @@ import { ApiResponse } from 'amadda-global-types';
 
 export const http = {
   get: async <Res = ApiResponse<unknown>>(url: string) => {
-    const result = await fetch(url, {
+    const response = await fetch(url, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
       },
     });
-    if (!result.ok) {
-      throw new Error(`status: ${result.status}, message: ${result.statusText}`);
+    const { code, message, data } = (await response.json()) satisfies Res;
+
+    if (response.status > 399) {
+      throw new Error(`status: ${code || response.status}, message: ${message || response.statusText}`);
     } else {
-      const { code, message, data } = (await result.json()) satisfies Res;
-      if (code > 399) throw new Error(`status: ${code}, message: ${message}`);
       return { status: code, message, data };
     }
   },
   post: async <Req = unknown, Res = ApiResponse<unknown>>(url: string, body?: Req) => {
-    const result = await fetch(url, {
+    const response = await fetch(url, {
       method: 'POST',
       body: JSON.stringify(body) || null,
       headers: {
         'Content-Type': 'application/json',
       },
     });
-    if (!result.ok) {
-      throw new Error(`status: ${result.status}, message: ${result.statusText}`);
+    const { code, message, data } = (await response.json()) satisfies Res;
+
+    if (response.status > 399) {
+      throw new Error(`status: ${code || response.status}, message: ${message || response.statusText}`);
     } else {
-      const { code, message, data } = (await result.json()) satisfies Res;
-      if (code > 399) throw new Error(`status: ${code}, message: ${message}`);
       return { status: code, message, data };
     }
   },
   put: async <Req = unknown, Res = ApiResponse<unknown>>(url: string, body?: Req) => {
-    const result = await fetch(url, {
+    const response = await fetch(url, {
       method: 'PUT',
       body: JSON.stringify(body) || null,
       headers: {
         'Content-Type': 'application/json',
       },
     });
-    if (!result.ok) {
-      throw new Error(`status: ${result.status}, message: ${result.statusText}`);
+    const { code, message, data } = (await response.json()) satisfies Res;
+
+    if (response.status > 399) {
+      throw new Error(`status: ${code || response.status}, message: ${message || response.statusText}`);
     } else {
-      const { code, message, data } = (await result.json()) satisfies Res;
-      if (code > 399) throw new Error(`status: ${code}, message: ${message}`);
       return { status: code, message, data };
     }
   },
   patch: async <Req = unknown, Res = ApiResponse<unknown>>(url: string, body?: Req) => {
-    const result = await fetch(url, {
+    const response = await fetch(url, {
       method: 'PATCH',
       body: JSON.stringify(body) || null,
       headers: {
         'Content-Type': 'application/json',
       },
     });
-    if (!result.ok) {
-      throw new Error(`status: ${result.status}, message: ${result.statusText}`);
+    const { code, message, data } = (await response.json()) satisfies Res;
+
+    if (response.status > 399) {
+      throw new Error(`status: ${code || response.status}, message: ${message || response.statusText}`);
     } else {
-      const { code, message, data } = (await result.json()) satisfies Res;
-      if (code > 399) throw new Error(`status: ${code}, message: ${message}`);
       return { status: code, message, data };
     }
   },
   delete: async <Res = ApiResponse<unknown>>(url: string) => {
-    const result = await fetch(url, {
+    const response = await fetch(url, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json',
       },
     });
-    if (!result.ok) {
-      throw new Error(`status: ${result.status}, message: ${result.statusText}`);
+    const { code, message, data } = (await response.json()) satisfies Res;
+
+    if (response.status > 399) {
+      throw new Error(`status: ${code || response.status}, message: ${message || response.statusText}`);
     } else {
-      const { code, message, data } = (await result.json()) satisfies Res;
-      if (code > 399) throw new Error(`status: ${code}, message: ${message}`);
       return { status: code, message, data };
     }
   },
