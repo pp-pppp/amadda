c092c1f466a39a72a4a469cac3ff2c731536cb95
brewcoldblue <seung.yoon.yu@gmail.com>
2024-01-01 15:48:30 +0900

    refactor: 알림 컴포넌트 Errorboundary 추가

--- frontend/_apps/notice/components/NoticeConfig/NoticeConfigFrame.tsx
+++ frontend/_apps/notice/components/NoticeConfig/NoticeConfigFrame.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import { Flex, H3, List, Spacing } from 'external-temporal';
+import { ErrorBoundary, Flex, H3, List, Spacing } from 'external-temporal';
 import { useGetNotice } from '@N/hooks/useGetNotice';
 import { NoticeToggle } from './NoticeToggle';
 
 export function NoticeConfigFrame() {
-  const { noticeList, setNoticeList, SWRerror } = useGetNotice();
+  const { noticeList, setNoticeList, error } = useGetNotice();
 
   return (
-    <Flex flexDirection="column" justifyContents="start">
-      <H3>알림 목록</H3>
-      <Spacing dir="v" size="0.25" />
-      <List.Ul>
-        {noticeList.map(n => (
-          <List.Li key={n.alarmSeq + n.content}>
-            <NoticeToggle {...n} />
-          </List.Li>
-        ))}
-      </List.Ul>
-    </Flex>
+    <div>
+      <Flex flexDirection="column" justifyContents="start">
+        <H3>알림 목록</H3>
+        <Spacing dir="v" size="0.25" />
+        <ErrorBoundary message="알람을 가져오는 데 실패했어요.">
+          <List.Ul>
+            {noticeList.map(n => (
+              <List.Li key={n.alarmSeq + n.content}>
+                <NoticeToggle {...n} />
+              </List.Li>
+            ))}
+          </List.Ul>
+        </ErrorBoundary>
+      </Flex>
+    </div>
   );
 }
