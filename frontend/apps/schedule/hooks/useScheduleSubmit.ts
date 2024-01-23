1c4447d58d56816ef265303acb279e8b5189e916
brewcoldblue <seung.yoon.yu@gmail.com>
2024-01-23 15:20:32 +0900

    refactor: put/post 요청에서 SWR 삭제

--- frontend/apps/schedule/hooks/useScheduleSubmit.ts
+++ frontend/apps/schedule/hooks/useScheduleSubmit.ts
@@ -1,8 +1,7 @@
-import { ScheduleCreateRequest, ScheduleUpdateRequest } from '@amadda/global-types';
-import { NextRouter, useRouter } from 'next/router';
+import { ScheduleUpdateRequest } from '@amadda/global-types';
+import { useRouter } from 'next/router';
 import { ScheduleEditFormProps } from '../components/ScheduleEdit/formdata';
 import { clientFetch } from '@amadda/fetch';
-import useSWR from 'swr';
 import { formToRequest } from '@SCH/utils/convertFormData';
 
 export function useScheduleSubmit() {
@@ -10,18 +9,16 @@ export function useScheduleSubmit() {
 
   async function submit(data: ScheduleEditFormProps) {
     const requestBody = formToRequest(data);
-    if (router.basePath.includes('edit')) {
+    if (router.basePath.includes('update')) {
       const { scheduleSeq } = router.query;
-
-      const { data, error } = useSWR(`${process.env.NEXT_PUBLIC_SCHEDULE}/api/schedule/${scheduleSeq}`, url =>
-        clientFetch.put<ScheduleUpdateRequest, string>(url, requestBody as ScheduleUpdateRequest)
+      const data = clientFetch.put<ScheduleUpdateRequest, string>(
+        `${process.env.NEXT_PUBLIC_SCHEDULE}/api/schedule/${scheduleSeq}`,
+        requestBody as ScheduleUpdateRequest
       );
       if (data) return router.push(`${process.env.NEXT_PUBLIC_SHELL}/schedule/${data}`);
       else return router.push(`${process.env.NEXT_PUBLIC_SHELL}/schedule`);
     } else {
-      const { data, error } = useSWR(`${process.env.NEXT_PUBLIC_SCHEDULE}/api/schedule`, url =>
-        clientFetch.post<ScheduleUpdateRequest, string>(url, requestBody as ScheduleUpdateRequest)
-      );
+      const data = clientFetch.post<ScheduleUpdateRequest, string>(`${process.env.NEXT_PUBLIC_SCHEDULE}/api/schedule`, requestBody as ScheduleUpdateRequest);
       if (data) return router.push(`${process.env.NEXT_PUBLIC_SHELL}/schedule/${data}`);
       else return router.push(`${process.env.NEXT_PUBLIC_SHELL}/schedule`);
     }
