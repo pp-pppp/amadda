2174d737ef4e3b5e76c1165d6c1045a6e7a09505
brewcoldblue <seung.yoon.yu@gmail.com>
2024-01-23 14:52:25 +0900

    fix: useScheduleSubmit 로직 변경

--- frontend/apps/schedule/hooks/useScheduleSubmit.ts
+++ frontend/apps/schedule/hooks/useScheduleSubmit.ts
@@ -1,58 +1,31 @@
 import { ScheduleCreateRequest, ScheduleUpdateRequest } from '@amadda/global-types';
-import { useRouter } from 'next/router';
+import { NextRouter, useRouter } from 'next/router';
 import { ScheduleEditFormProps } from '../components/ScheduleEdit/formdata';
 import { clientFetch } from '@amadda/fetch';
 import useSWR from 'swr';
+import { formToRequest } from '@SCH/utils/convertFormData';
 
-export async function useScheduleSubmit(data: ScheduleEditFormProps) {
+export function useScheduleSubmit() {
   const router = useRouter();
 
-  const request: ScheduleCreateRequest | ScheduleUpdateRequest = convert(data);
+  async function submit(data: ScheduleEditFormProps) {
+    const requestBody = formToRequest(data);
+    if (router.basePath.includes('edit')) {
+      const { scheduleSeq } = router.query;
 
-  if (router.basePath.includes('edit')) {
-    const { scheduleSeq } = router.query;
-
-    const { data, error } = useSWR(`${process.env.NEXT_PUBLIC_SCHEDULE}/api/schedule/${scheduleSeq}`, url =>
-      clientFetch.put<ScheduleUpdateRequest, string>(url, request as ScheduleUpdateRequest)
-    );
-    if (error) return router.push(`${process.env.NEXT_PUBLIC_SHELL}/schedule`);
-    if (data) return router.push(`${process.env.NEXT_PUBLIC_SHELL}/schedule/${data}`);
-  } else {
-    const { data, error } = useSWR(`${process.env.NEXT_PUBLIC_SCHEDULE}/api/schedule`, url =>
-      clientFetch.post<ScheduleUpdateRequest, string>(url, request as ScheduleUpdateRequest)
-    );
-    if (error) return router.push(`${process.env.NEXT_PUBLIC_SHELL}/schedule`);
-    if (data) return router.push(`${process.env.NEXT_PUBLIC_SHELL}/schedule/${data}`);
+      const { data, error } = useSWR(`${process.env.NEXT_PUBLIC_SCHEDULE}/api/schedule/${scheduleSeq}`, url =>
+        clientFetch.put<ScheduleUpdateRequest, string>(url, requestBody as ScheduleUpdateRequest)
+      );
+      if (data) return router.push(`${process.env.NEXT_PUBLIC_SHELL}/schedule/${data}`);
+      else return router.push(`${process.env.NEXT_PUBLIC_SHELL}/schedule`);
+    } else {
+      const { data, error } = useSWR(`${process.env.NEXT_PUBLIC_SCHEDULE}/api/schedule`, url =>
+        clientFetch.post<ScheduleUpdateRequest, string>(url, requestBody as ScheduleUpdateRequest)
+      );
+      if (data) return router.push(`${process.env.NEXT_PUBLIC_SHELL}/schedule/${data}`);
+      else return router.push(`${process.env.NEXT_PUBLIC_SHELL}/schedule`);
+    }
   }
-}
 
-function convert(formData: ScheduleEditFormProps): ScheduleCreateRequest | ScheduleUpdateRequest {
-  return formData.categorySeq
-    ? {
-        scheduleContent: formData.scheduleContent,
-        isTimeSelected: formData.isTimeSelected,
-        isDateSelected: formData.isDateSelected,
-        isAllday: formData.isAllday,
-        isAuthorizedAll: formData.isAllday,
-        scheduleStartAt: `${formData.startYear}-${formData.startMonth}-${formData.startDate} ${formData.startTime}:${formData.startMinute}`,
-        scheduleEndAt: `${formData.endYear}-${formData.endMonth}-${formData.endDate} ${formData.endTime}:${formData.endMinute}`,
-        participants: formData.participants,
-        alarmTime: String(formData.alarmTime),
-        scheduleName: formData.scheduleName,
-        scheduleMemo: formData.scheduleMemo,
-        categorySeq: Number(formData.categorySeq),
-      }
-    : {
-        scheduleContent: formData.scheduleContent,
-        isTimeSelected: formData.isTimeSelected,
-        isDateSelected: formData.isDateSelected,
-        isAllday: formData.isAllday,
-        isAuthorizedAll: formData.isAllday,
-        scheduleStartAt: `${formData.startYear}-${formData.startMonth}-${formData.startDate} ${formData.startTime}:${formData.startMinute}`,
-        scheduleEndAt: `${formData.endYear}-${formData.endMonth}-${formData.endDate} ${formData.endTime}:${formData.endMinute}`,
-        participants: formData.participants,
-        alarmTime: String(formData.alarmTime),
-        scheduleName: formData.scheduleName,
-        scheduleMemo: formData.scheduleMemo,
-      };
+  return submit;
 }
