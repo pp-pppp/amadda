5cbbc7945ad16b03fad1b5429e15a6a1ae57cf47
brewcoldblue <seung.yoon.yu@gmail.com>
2024-01-23 15:42:43 +0900

    fix: FAB 레이아웃 수정

--- frontend/apps/schedule/components/MobileMain/MobileMain.tsx
+++ frontend/apps/schedule/components/MobileMain/MobileMain.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { FABLAYOUT_MOBILE, INDEXLAYOUT_MOBILE } from '@SCH/layout/Layout.css';
 import { FAB, Segments } from '@amadda/external-temporal';
 import { MobileMonthlyPage } from './MobileMonthlyPage/MobileMonthlyPage';
@@ -9,11 +10,13 @@ export function MobileMain() {
   const router = useRouter();
 
   return (
-    <div className={INDEXLAYOUT_MOBILE}>
-      <Segments titles={[CALENDAR.TITLE_MONTHLY, CALENDAR.TITLE_UNSCHEUDLED]} pages={[<MobileMonthlyPage />, <MobileUnscheduledPage />]} />
+    <>
+      <div className={INDEXLAYOUT_MOBILE}>
+        <Segments titles={[CALENDAR.TITLE_MONTHLY, CALENDAR.TITLE_UNSCHEUDLED]} pages={[<MobileMonthlyPage />, <MobileUnscheduledPage />]} />
+      </div>
       <div className={FABLAYOUT_MOBILE} onClick={() => router.push(`${process.env.NEXT_PUBLIC_SHELL}/schedule/create`)}>
         <FAB />
       </div>
-    </div>
+    </>
   );
 }
