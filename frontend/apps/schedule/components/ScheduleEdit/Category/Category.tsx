5a139a450b3512830e28a7ebbc57c6a16924a26f
brewcoldblue <seung.yoon.yu@gmail.com>
2024-01-28 17:02:13 +0900

    docs: 불필요한 주석 삭제

--- frontend/apps/schedule/components/ScheduleEdit/Category/Category.tsx
+++ frontend/apps/schedule/components/ScheduleEdit/Category/Category.tsx
@@ -12,22 +12,6 @@ export function Category() {
       <Span>{CREATE.PLACEHOLDERS.CATEGORY}</Span>
       <Spacing dir="v" size="1" />
       <CategorySelectContainer categories={values.category} onCategoryClick={() => setValues({ ...values, categorySeq: values.categorySeq })} />
-      {/* <Spacing dir="v" size="0.5" />
-                TODO: 카테고리 추가 모달 추후 구현
-                <div>
-                  <Input
-                    type="text"
-                    id="categoryadd"
-                    onChange={e => setvalues.setCategoryInput(e.target.value)}
-                    onKeyDown={e => {
-                      e.key === 'enter' && fn.postCategory(values.categoryInput);
-                      setvalues.setCategoryInput('');
-                    }}
-                    value={values.categoryInput}
-                    name="categoryadd"
-                    placeholder="카테고리 추가"
-                  />
-                </div> */}
       <Spacing dir="v" size="2" />
     </>
   );
