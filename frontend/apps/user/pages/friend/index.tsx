ceb8d57c17811f3195243f01406559e8b57840df
brewcoldblue <seung.yoon.yu@gmail.com>
2024-01-31 16:37:40 +0900

    fix: import 변경 및 컴포넌트 구조 변경 대응

--- frontend/apps/user/pages/friend/index.tsx
+++ frontend/apps/user/pages/friend/index.tsx
@@ -1,42 +1,15 @@
 import { FriendFrame } from '@U/components/Friend/FriendFrame/FriendFrame';
-import { FriendGroups } from '@U/components/Friend/FriendGroups/FriendGroups';
-import { Friend } from '@U/components/Friend/Friend/Friend';
-import FRIENDS from '@U/constants/FRIENDS';
-import { useFriend } from '@U/hooks/useFriend';
-
 import { ErrorBoundary } from '@amadda/external-temporal';
-import { clientFetch } from '@amadda/fetch';
+import { FriendFrameHeader } from '@U/components/Friend/FriendFrame/FriendFrameHeader/FriendFrameHeader';
+import { FriendFrameBody } from '@U/components/Friend/FriendFrame/FriendFrameBody/FriendFrameBody';
 
 export default function FriendPage() {
-  const { data, isLoading, error } = useFriend();
   return (
     <div>
       <FriendFrame>
         <ErrorBoundary>
-          {data &&
-            data.map(g => {
-              if (g.groupSeq !== null) {
-                return (
-                  <FriendGroups key={g.groupSeq} groupSeq={g.groupSeq} groupName={g.groupName}>
-                    {g.groupMember.map(f => (
-                      <Friend key={f.userId} {...f} />
-                    ))}
-                  </FriendGroups>
-                );
-              } else
-                return (
-                  <FriendGroups key={g.groupSeq} groupSeq={g.groupSeq} groupName={FRIENDS.GROUPS.ALL_FRIENDS}>
-                    {g.groupMember.map(f => (
-                      <Friend
-                        key={f.userId}
-                        {...f}
-                        status="quit"
-                        onQuit={async () => await clientFetch.delete(`${process.env.NEXT_PUBLIC_USER}/friend/${f.userSeq}`)}
-                      />
-                    ))}
-                  </FriendGroups>
-                );
-            })}
+          <FriendFrameHeader />
+          <FriendFrameBody />
         </ErrorBoundary>
       </FriendFrame>
     </div>
