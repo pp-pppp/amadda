4bd9d0eb3b2269f095ed8eec860a07a9e836e539
brewcoldblue <seung.yoon.yu@gmail.com>
2024-02-25 17:46:58 +0900

    refactor: Block을 Body의 compound component 형태로 삽입

--- frontend/apps/user/components/Friend/FriendFrame/FriendFrameBody/FriendFrameBody.tsx
+++ frontend/apps/user/components/Friend/FriendFrame/FriendFrameBody/FriendFrameBody.tsx
@@ -1,22 +1,57 @@
 import { useFriendRouter } from '@U/store/friendRouter/useFriendRouter';
-import { FriendReadResultBlock } from './FriendReadResultBlock';
-import { FriendSearchResultBlock } from './FriendSearchResultBlock';
-import { FriendEditBlock } from './FriendEditBlock';
-import { FriendAddBlock } from './FriendAddBlock';
+import { useFriend, useSearchFriend } from '@U/hooks/useFriend';
+import { FriendLoading } from '../Fallback/FriendLoading';
+import { NoFriend } from '../Fallback/NoFriend';
+import { FriendGroupEdit } from './FriendGroup/FriendGroupEdit';
+import { FriendGroup } from './FriendGroup/FriendGroup';
+import { useFriendSearchStore } from '@U/store/friendSearchForm/useFriendSearchStore';
 
 export function FriendFrameBody() {
   const [PATH, PushToFriend] = useFriendRouter(state => [state.PATH, state.PushToFriend]);
 
   switch (PATH) {
     case 'ADD':
-      return <FriendAddBlock />;
+      return <FriendFrameBody.FriendAddBlock />;
     case 'READ':
-      return <FriendReadResultBlock />;
+      return <FriendFrameBody.FriendReadResultBlock />;
     case 'SEARCH':
-      return <FriendSearchResultBlock />;
+      return <FriendFrameBody.FriendSearchResultBlock />;
     case 'EDIT':
-      return <FriendEditBlock />;
+      return <FriendFrameBody.FriendEditBlock />;
     default:
       PushToFriend('READ');
   }
 }
+
+FriendFrameBody.FriendAddBlock = () => {
+  const { data, error, isLoading } = useFriend();
+
+  if (isLoading) return <FriendLoading />;
+  if (data?.length === 0) return <NoFriend />;
+  else return <>{data?.filter(group => <FriendGroupEdit {...group} />)}</>;
+};
+
+FriendFrameBody.FriendReadResultBlock = () => {
+  const { data, error, isLoading } = useFriend();
+
+  if (isLoading) return <FriendLoading />;
+  if (data?.length === 0) return <NoFriend />;
+  else return <>{data?.map(group => <FriendGroup {...group} />)}</>;
+};
+
+FriendFrameBody.FriendSearchResultBlock = () => {
+  const [searchKey] = useFriendSearchStore(state => [state.searchKey]);
+  const { data, error, isLoading } = useSearchFriend({ friendSearch: searchKey });
+
+  if (isLoading) return <FriendLoading />;
+  if (data?.length === 0) return <NoFriend />;
+  else return <>{data?.map(group => <FriendGroup {...group} />)}</>;
+};
+
+FriendFrameBody.FriendEditBlock = () => {
+  const { data, error, isLoading } = useFriend();
+
+  if (isLoading) return <FriendLoading />;
+  if (data?.length === 0) return <NoFriend />;
+  else return <>{data?.filter(group => <FriendGroupEdit {...group} />)}</>;
+};
