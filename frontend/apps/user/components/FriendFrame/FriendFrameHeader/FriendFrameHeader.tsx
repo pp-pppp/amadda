b9e3bf3641298aeedf2a8f4611709c37d8b32af6
brewcoldblue <seung.yoon.yu@gmail.com>
2024-03-03 12:00:32 +0900

    fix: hard code 된 제목을 constants에서 import하도록 변경

--- frontend/apps/user/components/FriendFrame/FriendFrameHeader/FriendFrameHeader.tsx
+++ frontend/apps/user/components/FriendFrame/FriendFrameHeader/FriendFrameHeader.tsx
@@ -1,11 +1,12 @@
 import { Flex, H2 } from '@amadda/external-temporal';
 import { FriendSearchInput } from './FriendSearchInput/FriendSearchInput';
-import { FriendRouteControl } from './FriendRouteControl/FriendRouteControl';
+import { FriendRouteControl } from '../FriendRouteControl/FriendRouteControl';
+import FRIENDS from '@U/constants/FRIENDS';
 
 export function FriendFrameHeader() {
   return (
     <Flex flexDirection="column" justifyContents="flexStart" alignItems="center">
-      <H2>친구</H2>
+      <H2>{FRIENDS.TITLE}</H2>
       <FriendRouteControl />
       <FriendSearchInput />
     </Flex>
