4ced7ad4d8f2801d4939926e9db3aa8a29bc0f60
brewcoldblue <seung.yoon.yu@gmail.com>
2024-03-07 20:56:23 +0900

    chore: user 도메인 분류

---
+++ frontend/apps/shell/constants/user/SIGNUP_TEXT.ts
@@ -0,0 +1,15 @@
+export default {
+  NICKNAME: '닉네임',
+  ID: '아이디',
+  NICKNAME_DESC: '닉네임은 한글, 영소문자, 숫자만 입력할 수 있어요',
+  NICKNAME_EMPTY: '닉네임을 입력해주세요',
+  ID_DESC: '아이디는 영소문자와 숫자만 입력할 수 있어요',
+  ID_EMPTY: '아이디를 입력해주세요',
+
+  NICKNAME_PLACEHOLDER: '닉네임은 실명 또는 실명에 가까운 이름을 추천해요',
+  ID_PLACEHOLDER: '아이디를 입력해주세요',
+
+  ID_DUPLICATE: '이미 존재하는 아이디에요',
+
+  SIGNUP_BUTTON: 'AMADDA 시작하기',
+};
