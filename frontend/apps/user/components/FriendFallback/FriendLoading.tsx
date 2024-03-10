43185d8b7e759ca38e6e70be45aa4b9ecf369398
brewcoldblue <seung.yoon.yu@gmail.com>
2024-03-10 18:44:27 +0900

    delete: 기존 파일 삭제

--- frontend/apps/user/components/FriendFallback/FriendLoading.tsx
+++
@@ -1,5 +0,0 @@
-import FRIENDS from '@U/constants/FRIENDS';
-
-export function FriendLoading() {
-  return <div>{FRIENDS.FALLBACKS.FRIEND_LOADING}</div>;
-}
--- frontend/apps/user/components/FriendFallback/NoFriend.tsx
+++
@@ -1,5 +0,0 @@
-import FRIENDS from '@U/constants/FRIENDS';
-
-export function NoFriend() {
-  return <div>{FRIENDS.FALLBACKS.NO_FRIEND}</div>;
-}
